6526a5896958fccb21baa5f2521a38121471f32b
Marcus Baker <marcus@lastcraft.com>
2006-10-09 11:13:37 +0000

    Still working on the callback docs

--- doc/source/make_bundled_docs.sh
+++ doc/source/make_bundled_docs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xsltproc bundled_docs.xslt quickstart.xml > ../index.html
 xsltproc bundled_docs.xslt cgreen_suites.xml > ../cgreen_suites.html
+xsltproc bundled_docs.xslt mock_callbacks.xml > ../mock_callbacks.html
