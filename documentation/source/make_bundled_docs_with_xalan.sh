1785e5f619e6ec1a244819ad528e57a364b1f64e
Marcus Baker <marcus@lastcraft.com>
2006-10-14 18:13:49 +0000

    More docs

--- documentation/source/make_bundled_docs_with_xalan.sh
+++ documentation/source/make_bundled_docs_with_xalan.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 Xalan -o ../index.html quickstart.xml bundled_docs.xslt
 Xalan -o ../cgreen_suites.html cgreen_suites.xml bundled_docs.xslt
+Xalan -o ../mock_callbacks.html mock_callbacks.xml bundled_docs.xslt
