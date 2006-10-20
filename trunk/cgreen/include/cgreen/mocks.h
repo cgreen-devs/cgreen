45e889893188f819218343030203a769f8b4dd60
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2006-10-20 20:43:01 +0000

    replacing sequences with proper mocks

--- trunk/cgreen/include/cgreen/mocks.h
+++ trunk/cgreen/include/cgreen/mocks.h
@@ -1,4 +1,12 @@
 #ifndef _MOCKS_HEADER_
 #define _MOCKS_HEADER_
 
+#define recorded_integer(i) _recorded_integer(__FILE__, __LINE__, i)
+#define recorded_string(s) _recorded_string(__FILE__, __LINE__, s)
+#define recorded_address(a) _recorded_address(__FILE__, __LINE__, a)
+
+void _recorded_integer(const char *file, int line, int integer);
+void _recorded_string(const char *file, int line, char *string);
+void _recorded_address(const char *file, int line, void *address);
+
 #endif
