054c2856ef0379fcf2b820d353cc6b5f32e04948
Marcus Baker <marcus@lastcraft.com>
2006-10-22 12:55:51 +0000

    Moved assertions to their own file

--- unit.h
+++ unit.h
@@ -9,13 +9,6 @@
 #define add_suite(owner, suite) _add_suite(owner, #suite, suite)
 #define setup(suite, function) _setup(suite, &function)
 #define teardown(suite, function) _teardown(suite, &function)
-#define assert_true(result, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, result, __VA_ARGS__)
-#define assert_false(result, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, ! result, __VA_ARGS__)
-#define assert_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, (tried == expected), __VA_ARGS__)
-#define assert_not_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, (tried != expected), __VA_ARGS__)
-#define assert_string_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, strcmp(tried, expected) == 0, __VA_ARGS__)
-#define assert_string_not_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, strcmp(tried, expected) != 0, __VA_ARGS__)
-#define recorded_integer(expected) _recorded_integer(__FILE__, __LINE__, expected)
 
 typedef struct _TestSuite TestSuite;
 
