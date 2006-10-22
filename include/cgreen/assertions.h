e0d2fd789460b080aeaa0518040323172666096e
Marcus Baker <marcus@lastcraft.com>
2006-10-22 17:29:49 +0000

    Needless NULL messages gone

--- include/cgreen/assertions.h
+++ include/cgreen/assertions.h
@@ -7,8 +7,8 @@
 #define assert_false(result) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, ! result, NULL)
 #define assert_equal(tried, expected) _assert_equal(__FILE__, __LINE__, (intptr_t)tried, (intptr_t)expected)
 #define assert_not_equal(tried, expected) _assert_not_equal(__FILE__, __LINE__, (intptr_t)tried, (intptr_t)expected)
-#define assert_string_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, strcmp(tried, expected) == 0, __VA_ARGS__)
-#define assert_string_not_equal(tried, expected, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, strcmp(tried, expected) != 0, __VA_ARGS__)
+#define assert_string_equal(tried, expected) _assert_string_equal(__FILE__, __LINE__, tried, expected)
+#define assert_string_not_equal(tried, expected) _assert_string_not_equal(__FILE__, __LINE__, tried, expected)
 
 #define assert_true_with_message(result, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, result, __VA_ARGS__)
 #define assert_false_with_message(result, ...) (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, ! result, __VA_ARGS__)
@@ -19,5 +19,7 @@
 
 void _assert_equal(const char *file, int line, intptr_t tried, intptr_t expected);
 void _assert_not_equal(const char *file, int line, intptr_t tried, intptr_t expected);
+void _assert_string_equal(const char *file, int line, const char *tried, const char *expected);
+void _assert_string_not_equal(const char *file, int line, const char *tried, const char *expected);
 
 #endif
