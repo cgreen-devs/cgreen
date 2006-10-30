6e9fc2cf1fd39d4584a4c01e0cd24a3f249289b5
Marcus Baker <marcus@lastcraft.com>
2006-10-30 13:26:23 +0000

    Working on reporter docs

--- documentation/source/make_bundled_docs.sh
+++ documentation/source/make_bundled_docs.sh
@@ -2,3 +2,4 @@
 xsltproc bundled_docs.xslt quickstart.xml > ../index.html
 xsltproc bundled_docs.xslt cgreen_suites.xml > ../cgreen_suites.html
 xsltproc bundled_docs.xslt mock_callbacks.xml > ../mock_callbacks.html
+xsltproc bundled_docs.xslt cgreen_reporter.xml > ../cgreen_reporter.html
