b94a55c6678622df994e39cfc0c6a5e3cc2dd90b
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2006-11-23 22:21:40 +0000

    working on mocks

--- trunk/cgreen/mocks.h
+++ trunk/cgreen/mocks.h
@@ -6,19 +6,21 @@
 
 #define checked_integer(i) _checked_integer(__FILE__, __LINE__, __func__, #i, (intptr_t)i)
 #define checked_string(s) _checked_string(__FILE__, __LINE__, __func__, #s, s)
-#define expect_exactly(f, ...) _expect(#f, __FILE__, __LINE__); f(__VA_ARGS__); _play()
-#define always_expect_exactly(f, ...) _always(); _expect(#f, __FILE__, __LINE__); f(__VA_ARGS__); _play()
-#define expect_never(f) _expect(#f, __FILE__, __LINE__); _expect_never(#f); _play()
+#define expect(f, ...) _expect(#f, __FILE__, __LINE__, __VA_ARGS__)
+#define expect_exactly(f, ...) _record(#f, __FILE__, __LINE__); f(__VA_ARGS__); _play()
+#define always_expect_exactly(f, ...) _always(); _record(#f, __FILE__, __LINE__); f(__VA_ARGS__); _play()
+#define expect_never(f) _record(#f, __FILE__, __LINE__); _expect_never(#f); _play()
 #define will_return(f, r) _will_return(#f, (intptr_t)r); _play()
 #define always_return(f, r) _always(); _will_return(#f, (intptr_t)r); _play()
-#define mock_exactly(f, r, ...) _expect(#f, __FILE__, __LINE__); f(__VA_ARGS__); _will_return(#f, (intptr_t)r); _play()
-#define always_mock_exactly(f, r, ...) _always(); _expect(#f, __FILE__, __LINE__); f(__VA_ARGS__); _will_return(#f, (intptr_t)r); _play()
+#define mock_exactly(f, r, ...) _record(#f, __FILE__, __LINE__); f(__VA_ARGS__); _will_return(#f, (intptr_t)r); _play()
+#define always_mock_exactly(f, r, ...) _always(); _record(#f, __FILE__, __LINE__); f(__VA_ARGS__); _will_return(#f, (intptr_t)r); _play()
 #define stubbed_result() _stubbed_result(__func__, __FILE__, __LINE__)
 
 void _checked_integer(const char *check_file, int check_line, const char *function, const char *parameter, intptr_t integer);
 void _checked_string(const char *check_file, int check_line, const char *function, const char *parameter, const char *string);
 intptr_t _stubbed_result(const char *function, const char *file, int line);
-void _expect(const char *function, const char *test_file, int test_line);
+void _expect(const char *function, const char *file, int line, ...);
+void _record(const char *function, const char *test_file, int test_line);
 void _expect_never(const char *function);
 void _play();
 void _will_return(const char *function, intptr_t result);
