6caf385c021fa507f7de5ea99ecdea986f52b844
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2006-11-30 00:41:37 +0000

    Working on is constraint

--- trunk/cgreen/include/cgreen/constraint.h
+++ trunk/cgreen/include/cgreen/constraint.h
@@ -7,8 +7,16 @@
 #define string_is(parameter, x) _string_is(#parameter, x)
 
 typedef struct _CgreenConstraint CgreenConstraint;
+struct _CgreenConstraint {
+    const char *parameter;
+    void (*destroy)(CgreenConstraint *);
+    int (*compare)(intptr_t);
+    intptr_t expected;
+};
 
-CgreenConstraint *_is(const char *parameter, intptr_t comparison);
-CgreenConstraint *_string_is(const char *parameter, char *comparison);
+CgreenConstraint *_is(const char *parameter, intptr_t expected);
+void is_constraint_destroy(CgreenConstraint *constraint);
+int is_constraint_compare(intptr_t comparison);
+CgreenConstraint *_string_is(const char *parameter, char *expected);
 
 #endif
--- trunk/cgreen/tests/constraint_tests.c
+++ trunk/cgreen/tests/constraint_tests.c
@@ -2,7 +2,13 @@
 #include "../constraint.h"
 #include <stdlib.h>
 
+static void can_construct_and_destroy_is_constraint() {
+    CgreenConstraint *constraint = is(label, 37);
+    //(*constraint->destroy)(constraint);
+}
+
 TestSuite *constraint_tests() {
     TestSuite *suite = create_test_suite();
+    add_test(suite, can_construct_and_destroy_is_constraint);
     return suite;
 }
