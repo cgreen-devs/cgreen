6c8021a62fd09b82356fd8a006ac033b9d46a105
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2006-12-02 15:05:13 +0000

    Working on catching missing expectations

--- trunk/cgreen/tests/constraint_tests.c
+++ trunk/cgreen/tests/constraint_tests.c
@@ -3,43 +3,43 @@
 #include <stdlib.h>
 
 static void can_construct_and_destroy_an_want_constraint() {
-    CgreenConstraint *any_old_want = want(label, 37);
+    Constraint *any_old_want = want(label, 37);
     destroy_constraint(any_old_want);
 }
 
 static void equal_integers_compare_true_with_a_want_constraint() {
-    CgreenConstraint *want_37 = want(label, 37);
+    Constraint *want_37 = want(label, 37);
     assert_equal(compare_constraint(want_37, 37), 1);
     destroy_constraint(want_37);
 }
 
 static void equal_pointers_compare_true_with_a_want_constraint() {
-    CgreenConstraint *want_pointed_at_37 = want(label, (void *)37);
+    Constraint *want_pointed_at_37 = want(label, (void *)37);
     assert_equal(compare_constraint(want_pointed_at_37, (void *)37), 1);
     assert_equal(compare_constraint(want_pointed_at_37, (void *)36), 0);
     destroy_constraint(want_pointed_at_37);
 }
 
 static void can_construct_and_destroy_an_want_string_constraint() {
-    CgreenConstraint *any_old_string_want = want_string(label, "Hello");
+    Constraint *any_old_string_want = want_string(label, "Hello");
     destroy_constraint(any_old_string_want);
 }
 
 static void can_compare_strings_as_equal() {
-    CgreenConstraint *want_hello = want_string(label, "Hello");
+    Constraint *want_hello = want_string(label, "Hello");
     assert_equal(compare_constraint(want_hello, "Hello"), 1);
     assert_equal(compare_constraint(want_hello, "Goodbye"), 0);
     destroy_constraint(want_hello);
 }
 
 static void can_compare_null_strings_as_well_as_real_ones() {
-    CgreenConstraint *want_hello = want_string(label, "Hello");
+    Constraint *want_hello = want_string(label, "Hello");
     assert_equal(compare_constraint(want_hello, NULL), 0);
     destroy_constraint(want_hello);
 }
 
 static void can_expect_null_strings_as_well_as_real_ones() {
-    CgreenConstraint *want_hello = want_string(label, NULL);
+    Constraint *want_hello = want_string(label, NULL);
     assert_equal(compare_constraint(want_hello, NULL), 1);
     assert_equal(compare_constraint(want_hello, "Hello"), 0);
     destroy_constraint(want_hello);
