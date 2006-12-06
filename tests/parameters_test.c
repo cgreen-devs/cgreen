6399f952e36c63da989ee4cf80223ab9a72555b3
Marcus Baker <marcus@lastcraft.com>
2006-12-06 14:29:35 +0000

    Working on parameter parsing

--- tests/parameters_test.c
+++ tests/parameters_test.c
@@ -1,6 +1,15 @@
 #include "../cgreen.h"
+#include "../vector.h"
+#include "../parameters.h"
+
+static void can_create_vector_of_no_parameters_and_destroy_it() {
+    Vector *names = create_vector_of_names("");
+    assert_equal(vector_size(names), 0);
+    destroy_vector(names);
+}
 
 TestSuite *parameter_tests() {
     TestSuite *suite = create_test_suite();
+    add_test(suite, can_create_vector_of_no_parameters_and_destroy_it);
     return suite;
 }
