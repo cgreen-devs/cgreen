30e657feafe9666a8bcbe0bfb2d66b5b6b44a195
Marcus Baker <marcus@lastcraft.com>
2006-12-06 16:06:39 +0000

    Added parameter parser

--- parameters.c
+++ parameters.c
@@ -1,7 +1,46 @@
 #include "parameters.h"
 #include "vector.h"
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+static char *tokenise_by_commas_and_whitespace(char *list);
+static char *skip_nulls(char *pointer);
+static char *end_of_token(char *token);
 
 Vector *create_vector_of_names(const char *parameters) {
-    return create_vector(&free);
+    Vector *names = create_vector(&free);
+    if ((parameters == NULL) || (strlen(parameters) == 0)) {
+        return names;
+    }
+    char *tokens = tokenise_by_commas_and_whitespace(strdup(parameters));
+    char *token = tokens;
+    while (token < tokens + strlen(parameters)) {
+        token = skip_nulls(token);
+        vector_add(names, strdup(token));
+        token = end_of_token(token);
+    }
+    free(tokens);
+    return names;
+}
+
+static char *tokenise_by_commas_and_whitespace(char *list) {
+    int i, length;
+    for (i = 0, length = strlen(list); i < length; i++) {
+        if (isspace(list[i]) || list[i] == ',') {
+            list[i] = '\0';
+        }
+    }
+    return list;
+}
+
+static char *skip_nulls(char *pointer) {
+    while (*pointer == '\0') {
+        pointer++;
+    }
+    return pointer;
+}
+
+static char *end_of_token(char *token) {
+    return token + strlen(token);
 }
--- tests/parameters_test.c
+++ tests/parameters_test.c
@@ -1,15 +1,75 @@
 #include "../cgreen.h"
 #include "../vector.h"
 #include "../parameters.h"
+#include <stdlib.h>
+
+static Vector *names = NULL;
+
+static void destroy_names() {
+    destroy_vector(names);
+    names = NULL;
+}
 
 static void can_create_vector_of_no_parameters_and_destroy_it() {
-    Vector *names = create_vector_of_names("");
+    names = create_vector_of_names("");
     assert_equal(vector_size(names), 0);
-    destroy_vector(names);
+}
+
+static void can_read_single_parameter() {
+    names = create_vector_of_names("a");
+    assert_equal(vector_size(names), 1);
+    assert_string_equal(vector_get(names, 0), "a");
+}
+
+static void can_read_two_parameters() {
+    names = create_vector_of_names("a, b");
+    assert_equal(vector_size(names), 2);
+    assert_string_equal(vector_get(names, 0), "a");
+    assert_string_equal(vector_get(names, 1), "b");
+}
+
+static void can_read_three_parameters() {
+    names = create_vector_of_names("a, b, c");
+    assert_equal(vector_size(names), 3);
+    assert_string_equal(vector_get(names, 0), "a");
+    assert_string_equal(vector_get(names, 1), "b");
+    assert_string_equal(vector_get(names, 2), "c");
+}
+
+static void can_read_two_parameters_without_spaces() {
+    names = create_vector_of_names("a,b");
+    assert_string_equal(vector_get(names, 0), "a");
+    assert_string_equal(vector_get(names, 1), "b");
+}
+
+static void can_read_long_parameters() {
+    names = create_vector_of_names("abacus, banana");
+    assert_string_equal(vector_get(names, 0), "abacus");
+    assert_string_equal(vector_get(names, 1), "banana");
+}
+
+static void can_read_long_parameters_with_funky_names() {
+    names = create_vector_of_names("a_b-c+d, +a-d_c/d");
+    assert_string_equal(vector_get(names, 0), "a_b-c+d");
+    assert_string_equal(vector_get(names, 1), "+a-d_c/d");
+}
+
+static void can_read_two_parameters_with_varied_whitespace() {
+    names = create_vector_of_names("\ra\t,\nb\t\t\t");
+    assert_string_equal(vector_get(names, 0), "a");
+    assert_string_equal(vector_get(names, 1), "b");
 }
 
 TestSuite *parameter_tests() {
     TestSuite *suite = create_test_suite();
+    teardown(suite, destroy_names);
     add_test(suite, can_create_vector_of_no_parameters_and_destroy_it);
+    add_test(suite, can_read_single_parameter);
+    add_test(suite, can_read_two_parameters);
+    add_test(suite, can_read_three_parameters);
+    add_test(suite, can_read_two_parameters_without_spaces);
+    add_test(suite, can_read_long_parameters);
+    add_test(suite, can_read_long_parameters_with_funky_names);
+    add_test(suite, can_read_two_parameters_with_varied_whitespace);
     return suite;
 }
