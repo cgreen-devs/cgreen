b058852c1d712c22400b3880ef9772b46946aab1
Marcus Baker <marcus@lastcraft.com>
2006-12-08 10:48:19 +0000

    Working on constraints sending failure messages

--- constraint.c
+++ constraint.c
@@ -1,6 +1,7 @@
 #include "constraint.h"
 #include <inttypes.h>
 #include <stdlib.h>
+#include <string.h>
 
 void destroy_empty_constraint(Constraint *constraint);
 int compare_want(Constraint *constraint, intptr_t comparison);
@@ -12,12 +13,17 @@ void destroy_constraint(void *abstract) {
     (*constraint->destroy)(constraint);
 }
 
+int is_constraint_parameter(Constraint *constraint, const char *parameter) {
+    return strcmp(constraint->parameter, parameter) == 0;
+}
+
 int _compare_constraint(Constraint *constraint, intptr_t comparison) {
     (*constraint->compare)(constraint, comparison);
 }
 
 Constraint *_want(const char *parameter, intptr_t expected) {
     Constraint *constraint = create_constraint(parameter);
+    constraint->parameter = parameter;
     constraint->compare = &compare_want;
     constraint->expected = expected;
     return constraint;
