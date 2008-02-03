7763960ec1bacfcd5ab5dc0e4ef01d85a1350b37
Marcus Baker <marcus@lastcraft.com>
2008-02-03 00:12:53 +0000

    The start of teh big renaming

--- unit.h
+++ unit.h
@@ -1,23 +1,23 @@
-#ifndef _UNIT_HEADER_
-#define _UNIT_HEADER_
+#ifndef UNIT_HEADER
+#define UNIT_HEADER
 
 #include "reporter.h"
 #include "mocks.h"
 
 #define create_test_suite() create_named_test_suite(__func__)
-#define add_test(suite, test) _add_test(suite, #test, &test)
-#define add_suite(owner, suite) _add_suite(owner, #suite, suite)
-#define setup(suite, function) _setup(suite, &function)
-#define teardown(suite, function) _teardown(suite, &function)
+#define add_test(suite, test) add_test_(suite, #test, &test)
+#define add_suite(owner, suite) add_suite_(owner, #suite, suite)
+#define setup(suite, function) setup_(suite, &function)
+#define teardown(suite, function) teardown_(suite, &function)
 
-typedef struct _TestSuite TestSuite;
+typedef struct TestSuite_ TestSuite;
 
 TestSuite *create_named_test_suite(const char *name);
 void destroy_test_suite(TestSuite *suite);
-void _add_test(TestSuite *suite, char *name, void (*test)(TestReporter *));
-void _add_suite(TestSuite *owner, char *name, TestSuite *suite);
-void _setup(TestSuite *suite, void (*set_up)());
-void _teardown(TestSuite *suite, void (*tear_down)());
+void add_test_(TestSuite *suite, char *name, void (*test)(TestReporter *));
+void add_suite_(TestSuite *owner, char *name, TestSuite *suite);
+void setup_(TestSuite *suite, void (*set_up)());
+void teardown_(TestSuite *suite, void (*tear_down)());
 void die_in(unsigned int seconds);
 int run_test_suite(TestSuite *suite, TestReporter *reporter);
 int run_single_test(TestSuite *suite, char *test, TestReporter *reporter);
