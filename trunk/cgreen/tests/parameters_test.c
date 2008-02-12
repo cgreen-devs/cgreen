9506b6213a644cf244222fb9ef25d8316a603d41
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2008-02-12 01:12:49 +0000

    Adding Ensure macro to mark tests

--- trunk/cgreen/tests/parameters_test.c
+++ trunk/cgreen/tests/parameters_test.c
@@ -5,30 +5,30 @@
 
 static CgreenVector *names = NULL;
 
-static void destroy_names() {
+Ensure destroy_names() {
     destroy_cgreen_vector(names);
     names = NULL;
 }
 
-static void can_create_vector_of_no_parameters_and_destroy_it() {
+Ensure can_create_vector_of_no_parameters_and_destroy_it() {
     names = create_vector_of_names("");
     assert_equal(cgreen_vector_size(names), 0);
 }
 
-static void can_read_single_parameter() {
+Ensure can_read_single_parameter() {
     names = create_vector_of_names("a");
     assert_equal(cgreen_vector_size(names), 1);
     assert_string_equal(cgreen_vector_get(names, 0), "a");
 }
 
-static void can_read_two_parameters() {
+Ensure can_read_two_parameters() {
     names = create_vector_of_names("a, b");
     assert_equal(cgreen_vector_size(names), 2);
     assert_string_equal(cgreen_vector_get(names, 0), "a");
     assert_string_equal(cgreen_vector_get(names, 1), "b");
 }
 
-static void can_read_three_parameters() {
+Ensure can_read_three_parameters() {
     names = create_vector_of_names("a, b, c");
     assert_equal(cgreen_vector_size(names), 3);
     assert_string_equal(cgreen_vector_get(names, 0), "a");
@@ -36,25 +36,25 @@ static void can_read_three_parameters() {
     assert_string_equal(cgreen_vector_get(names, 2), "c");
 }
 
-static void can_read_two_parameters_without_spaces() {
+Ensure can_read_two_parameters_without_spaces() {
     names = create_vector_of_names("a,b");
     assert_string_equal(cgreen_vector_get(names, 0), "a");
     assert_string_equal(cgreen_vector_get(names, 1), "b");
 }
 
-static void can_read_long_parameters() {
+Ensure can_read_long_parameters() {
     names = create_vector_of_names("abacus, banana");
     assert_string_equal(cgreen_vector_get(names, 0), "abacus");
     assert_string_equal(cgreen_vector_get(names, 1), "banana");
 }
 
-static void can_read_long_parameters_with_funky_names() {
+Ensure can_read_long_parameters_with_funky_names() {
     names = create_vector_of_names("a_b-c+d, +a-d_c/d");
     assert_string_equal(cgreen_vector_get(names, 0), "a_b-c+d");
     assert_string_equal(cgreen_vector_get(names, 1), "+a-d_c/d");
 }
 
-static void can_read_two_parameters_with_varied_whitespace() {
+Ensure can_read_two_parameters_with_varied_whitespace() {
     names = create_vector_of_names("\ra\t,\nb\t\t\t");
     assert_string_equal(cgreen_vector_get(names, 0), "a");
     assert_string_equal(cgreen_vector_get(names, 1), "b");
