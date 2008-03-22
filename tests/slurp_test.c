b32b3e3773a3d6b389efa5a35cd908b2f4ecb224
Marcus Baker <marcus@lastcraft.com>
2008-03-22 22:20:23 +0000

    Implementing slurp

--- tests/slurp_test.c
+++ tests/slurp_test.c
@@ -4,16 +4,21 @@
 #include <string.h>
 
 Ensure missing_file_gives_null() {
-    assert_equal(slurp("tests/samples/not_there"), NULL);
+    assert_equal(slurp("tests/samples/not_there", 1024), NULL);
 }
 
 Ensure whole_file_can_be_read() {
-    assert_string_equal(slurp("tests/samples/some_file"), "Just some stuff\n");
+    assert_string_equal(slurp("tests/samples/some_file", 1024), "Just some stuff\n");
+}
+
+Ensure whole_file_can_be_read_in_multiple_reads() {
+    assert_string_equal(slurp("tests/samples/some_file", 1), "Just some stuff\n");
 }
 
 TestSuite *slurp_tests() {
     TestSuite *suite = create_test_suite();
     add_test(suite, missing_file_gives_null);
-    //add_test(suite, whole_file_can_be_read);
+    add_test(suite, whole_file_can_be_read);
+    add_test(suite, whole_file_can_be_read_in_multiple_reads);
     return suite;
 }
