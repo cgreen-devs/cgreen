1e051e2e252c62ff05dea9add856d84606ffd914
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2008-03-22 22:20:23 +0000

    Implementing slurp

--- trunk/cgreen/slurp.c
+++ trunk/cgreen/slurp.c
@@ -1,6 +1,28 @@
 #include "slurp.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-char *slurp(const char *file_name) {
-    return NULL;
+static char *read_all(FILE *file, int gulp);
+
+char *slurp(const char *file_name, int gulp) {
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+    char *content = read_all(file, gulp);
+    fclose(file);
+    return content;
+}
+
+static char *read_all(FILE *file, int gulp) {
+    char *content = (char *)malloc(gulp * sizeof(char));
+    char *block = content;
+    for ( ; ; ) {
+        if (fgets(block, gulp + 1, file) == NULL) {
+            break;
+        }
+        block += gulp;
+        content = (char *)realloc(content, (block - content) * sizeof(char));
+    }
+    return content;
 }
--- trunk/cgreen/slurp.h
+++ trunk/cgreen/slurp.h
@@ -1,6 +1,6 @@
 #ifndef SLURP_HEADER
 #define SLURP_HEADER
 
-char *slurp(const char *file_name);
+char *slurp(const char *file_name, int gulp);
 
 #endif
