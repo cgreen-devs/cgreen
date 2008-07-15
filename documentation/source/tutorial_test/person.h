c33f0500a3c80185a8e010fc5bf00c3cc5cf45be
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2008-07-15 12:09:30 +0000

    move into doc

--- documentation/source/tutorial_test/person.h
+++
@@ -1,9 +0,0 @@
-typedef struct _Person {
-    char *name;
-} Person;
-
-Person *create_person();
-Person *find_person_by_name(const char *);
-void set_person_name(Person *, const char *);
-char *get_person_name(Person *);
-int save_person(Person *);
--- documentation/source/tutorial_test/xml_reporter.h
+++
@@ -1,8 +0,0 @@
-#ifndef _XML_REPORTER_HEADER_
-#define _XML_REPORTER_HEADER_
-
-#include "cgreen/reporter.h"
-
-TestReporter *create_xml_reporter();
-
-#endif
