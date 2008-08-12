92fb7314f941f8f85a3ee27195c0f0907feb0dbd
lastcraft <lastcraft@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2008-08-12 16:28:55 +0000

    Adding auto-stripping of double annotations on mocks - first get the parameter parsing working

--- trunk/cgreen/tests/parameters_test.c
+++ trunk/cgreen/tests/parameters_test.c
@@ -60,24 +60,28 @@ Ensure can_read_two_parameters_with_varied_whitespace() {
     assert_string_equal(cgreen_vector_get(names, 1), "b");
 }
 
+Ensure can_strip_box_double_to_leave_original_name() {
+    names = create_vector_of_names("box_double(a)");
+    assert_string_equal(cgreen_vector_get(names, 0), "a");
+}
+
+Ensure can_strip_d_macro_to_leave_original_name() {
+    names = create_vector_of_names("d(a)");
+    assert_string_equal(cgreen_vector_get(names, 0), "a");
+}
+
 TestSuite *parameter_tests() {
     TestSuite *suite = create_test_suite();
     teardown(suite, destroy_names);
-    //add_test(suite, can_create_vector_of_no_parameters_and_destroy_it);
-    //add_test(suite, can_read_single_parameter);
-    //add_test(suite, can_read_two_parameters);
-    //add_test(suite, can_read_three_parameters);
-    //add_test(suite, can_read_two_parameters_without_spaces);
-    //add_test(suite, can_read_long_parameters);
-    //add_test(suite, can_read_long_parameters_with_funky_names);
-    //add_test(suite, can_read_two_parameters_with_varied_whitespace);
-    add_tests(suite, can_create_vector_of_no_parameters_and_destroy_it,
-                     can_read_single_parameter,
-                     can_read_two_parameters,
-                     can_read_three_parameters,
-                     can_read_two_parameters_without_spaces,
-                     can_read_long_parameters,
-                     can_read_long_parameters_with_funky_names,
-                     can_read_two_parameters_with_varied_whitespace);
+    add_test(suite, can_create_vector_of_no_parameters_and_destroy_it);
+    add_test(suite, can_read_single_parameter);
+    add_test(suite, can_read_two_parameters);
+    add_test(suite, can_read_three_parameters);
+    add_test(suite, can_read_two_parameters_without_spaces);
+    add_test(suite, can_read_long_parameters);
+    add_test(suite, can_read_long_parameters_with_funky_names);
+    add_test(suite, can_read_two_parameters_with_varied_whitespace);
+    add_test(suite, can_strip_box_double_to_leave_original_name);
+    add_test(suite, can_strip_d_macro_to_leave_original_name);
     return suite;
 }
