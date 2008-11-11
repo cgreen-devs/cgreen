ecb76d0b79780006351af70a384918232812b4df
joaohf <joaohf@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2008-11-11 17:02:23 +0000

    * Reverted to a simple Makefile.

--- trunk/cgreen/unbuildit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find . -name Makefile.in -exec rm -v {} \;
-find . -name Makefile -exec rm -v {} \;
-rm aclocal.m4 config.guess config.sub configure install-sh ltconfig ltmain.sh missing mkinstalldirs depcomp
-rm Makefile config.cache config.log config.status libtool
-rm config.h.in stamp-h.in
