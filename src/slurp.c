5448cd92876ce7e736cef8a3e05881e24f846b73
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2008-12-17 18:54:28 +0000

    discards p pointer

--- src/slurp.c
+++ src/slurp.c
@@ -19,9 +19,7 @@ static char *read_all(FILE *file, int gulp) {
     char *content = (char *)malloc(0);
     int sblock = (gulp + 1) * sizeof(char);
     char *block = (char *)malloc(sblock);
-    int len = 0;
-    int add = 0;
-    char *p;
+    int len, add = 0;
     for ( ; ; ) {
         if (fgets(block, sblock, file) == NULL) {
             break;
@@ -30,11 +28,10 @@ static char *read_all(FILE *file, int gulp) {
         len  = strlen(block);
 	add += len;
 
-        p = (char *)realloc(content, add + 1);
-	if (p == NULL) {
+        content = (char *)realloc(content, add + 1);
+	if (content == NULL) {
 		exit(1);
 	}
-        content = p;
 
 	strncat(content, block, len);
     }
