13ecdf42199f4c0a6c3046b0d7ed650d1c29d683
gladiac <gladiac@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2009-05-19 09:02:45 +0000

    Add doxygen support.
    If doxygen is installed 'make doc' is available and will create the
    doxygen documentation.

--- trunk/cgreen/include/cgreen/unit.h
+++ trunk/cgreen/include/cgreen/unit.h
@@ -4,6 +4,24 @@
 #include <cgreen/reporter.h>
 #include <cgreen/mocks.h>
 
+/**
+ * @defgroup unit_tests Unit Test Functions
+ *
+ * @brief Functions to create a Test Suite and adding tests.
+ */
+
+/**
+ * @addtogroup unit_tests
+ * @{
+ */
+
+/**
+ * @brief Creates a new test suite.
+ *
+ * @return A newly allocated test suite, NULL on error.
+ *
+ * @see destroy_test_suite()
+ */
 #define create_test_suite() create_named_test_suite(__func__)
 #define add_test(suite, test) add_test_(suite, #test, &test)
 #define add_tests(suite, ...) add_tests_(suite, #__VA_ARGS__, (CgreenTest *)__VA_ARGS__ +0)
@@ -15,7 +33,22 @@
 typedef struct TestSuite_ TestSuite;
 typedef void CgreenTest();
 
+/**
+ * @brief Create a new test suite with a special name.
+ *
+ * @param  name         The name of the test suite.
+ *
+ * @return A newly allocated test suite, NULL on error.
+ *
+ * @see destroy_test_suite()
+ */
 TestSuite *create_named_test_suite(const char *name);
+
+/**
+ * @brief Free the memory of a test suite.
+ *
+ * @param  suite        The test suite to free.
+ */
 void destroy_test_suite(TestSuite *suite);
 void add_test_(TestSuite *suite, char *name, CgreenTest *test);
 void add_tests_(TestSuite *suite, const char *names, ...);
@@ -26,4 +59,7 @@ void die_in(unsigned int seconds);
 int run_test_suite(TestSuite *suite, TestReporter *reporter);
 int run_single_test(TestSuite *suite, char *test, TestReporter *reporter);
 
+/**
+ * @}
+ */
 #endif
