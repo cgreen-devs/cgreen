a74fd41a7a8345c89a1a5d3f3c45a37b61610710
joaohf <joaohf@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2010-02-17 01:01:56 +0000

    add cdash dashboard

--- trunk/cgreen/cmake/Modules/MacroAddUnitTest.cmake
+++ trunk/cgreen/cmake/Modules/MacroAddUnitTest.cmake
@@ -14,6 +14,9 @@ set(CMAKE_SHARED_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE 
 set(CMAKE_MODULE_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags")
 set(CMAKE_EXEC_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags")
 
+# custom options
+configure_file(tests/CTestCustom.cmake ${CMAKE_BINARY_DIR}/CTestCustom.cmake COPYONLY)
+
 macro (MACRO_ADD_UNIT_TEST _testName _testSource)
   add_executable(${_testName} ${_testSource})
   target_link_libraries(${_testName} ${ARGN})
@@ -24,4 +27,3 @@ else()
 endif (WIN32)
 
 endmacro (MACRO_ADD_UNIT_TEST)
-
