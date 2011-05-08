3894de49c632d199233b8bae7540f092f0a8e7d7
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2011-05-08 21:07:11 +0000

    Ability to build a C++ version of cgreen, by Colm Dougan

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,3 +1,5 @@
 option(WITH_STATIC_LIBRARY "Build with a static library" ON)
 option(WITH_UNIT_TESTS "Build unit tests" ON)
 option(WITH_DOCS "Build with documentation" OFF)
+option(WITH_CXX "Build the C++ library" OFF)
+
