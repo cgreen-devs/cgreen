39812c88b5e4f2eeae8a764d2afd35ff12ba921a
thomasnilsson <thomasnilsson@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2012-08-19 05:40:02 +0000

    Fixed CMAKE recepies for modern Cmake and Cygwin Cleared up basic install instructions to be somewhat correct

--- trunk/cgreen/DefineOptions.cmake
+++ trunk/cgreen/DefineOptions.cmake
@@ -2,4 +2,3 @@ option(WITH_STATIC_LIBRARY "Build with a static library" OFF)
 option(WITH_UNIT_TESTS "Build unit tests" ON)
 option(WITH_DOCS "Build with documentation" OFF)
 option(WITH_CXX "Build the C++ library" OFF)
-
