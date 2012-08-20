f2620aa3b26ff6dd114ac85f3aced755ff303379
Thomas Nilsson <thoni56@users.noreply.github.com>
2012-08-20 00:23:49 +0000

    Added a script to convert older test cases to new syntax Bulk supplied by Colm Dougan

---
+++ contrib/upgrade/upgrade.sh
@@ -0,0 +1,10 @@
+# Perl-script to upgrade from pre-beta versions
+# Initial version by Colm Dougan, contributed by Thomas Nilsson
+perl -pi -e 's/\bEnsure\s+(\w+)\s*\(\)/Ensure ($1)/' $@
+perl -pi -e 's/\bfail\(\)/fail("expected exception")/' $@
+perl -pi -e 's/\bexpect_call/expect/' $@
+perl -pi -e 's/\bwant\((\w+), ([^)]+)/when($1, is_equal_to($2)/' $@
+perl -pi -e 's/\bwill_respond\((\w+),\ ([^,\)]+)/expect($1, will_return($2)/' $@
+perl -pi -e 's/\bwant_non_null_?\(\"(\w+)\"\)/when($1, is_non_null)/' $@
+perl -pi -e 's/\bsetup\(/set_setup(/g' $@
+perl -pi -e 's/\bteardown\(/set_teardown(/g' $@
