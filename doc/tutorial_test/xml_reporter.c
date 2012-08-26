9bab878b70bdc575226992f92f69188d494a3581
Thomas Nilsson <thoni56@users.noreply.github.com>
2012-08-26 23:00:55 +0000

    Fixed up the quick start chapter on changing reporting using the xml-reporter

--- doc/tutorial_test/xml_reporter.c
+++ doc/tutorial_test/xml_reporter.c
@@ -1,23 +1,22 @@
 #include "xml_reporter.h"
-#include <cgreen/reporter.h>
-#include <cgreen/breadcrumb.h>
-#include <stdio.h>
-#include <stdarg.h>
+#include "cgreen/reporter.h"
+#include "cgreen/breadcrumb.h"
 
-static indent(TestReporter *reporter);
-static void xml_reporter_start(TestReporter *reporter, const char *name);
-static void xml_reporter_finish(TestReporter *reporter, const char *name);
+#include <stdio.h>
+
+static void xml_reporter_start_suite(TestReporter *reporter, const char *name, int count);
+static void xml_reporter_start_test(TestReporter *reporter, const char *name);
+static void xml_reporter_finish_test(TestReporter *reporter, const char *filename, int line);
+static void xml_reporter_finish_suite(TestReporter *reporter, const char *filename, int line);
 static void xml_show_fail(TestReporter *reporter, const char *file, int line, const char *message, va_list arguments);
-static void xml_show_incomplete(TestReporter *reporter, const char *name);
 
 TestReporter *create_xml_reporter() {
     TestReporter *reporter = create_reporter();
-	reporter->start_suite = &xml_reporter_start;
-	reporter->start_test = &xml_reporter_start;
-	reporter->show_fail = &xml_show_fail;
-	reporter->show_incomplete = &xml_show_incomplete;
-	reporter->finish_test = &xml_reporter_finish;
-	reporter->finish_suite = &xml_reporter_finish;
+    reporter->start_suite = &xml_reporter_start_suite;
+    reporter->start_test = &xml_reporter_start_test;
+    reporter->show_fail = &xml_show_fail;
+    reporter->finish_test = &xml_reporter_finish_test;
+    reporter->finish_suite = &xml_reporter_finish_suite;
     return reporter;
 }
 
@@ -28,39 +27,51 @@ static indent(TestReporter *reporter) {
     }
 }
 
-static void xml_reporter_start(TestReporter *reporter, const char *name) {
+static void xml_reporter_start_suite(TestReporter *reporter, const char *name, int count) {
     if (get_breadcrumb_depth((CgreenBreadcrumb *)reporter->breadcrumb) == 0) {
         printf("<?xml?>\n");
     }
-	indent(reporter);
-	printf("<test name=\"%s\">\n", name);
-	reporter_start(reporter, name);
+    indent(reporter);
+    printf("<suite name=\"%s\">\n", name);
+    reporter_start(reporter, name);
 }
 
-static void xml_reporter_finish(TestReporter *reporter, const char *name) {
-	reporter_finish(reporter, name);
-	indent(reporter);
-	printf("</test>\n");
+static void xml_reporter_start_test(TestReporter *reporter, const char *name) {
+    indent(reporter);
+    printf("<test name=\"%s\">\n", name);
+    reporter_start(reporter, name);
 }
 
 static void xml_show_fail(TestReporter *reporter, const char *file, int line, const char *message, va_list arguments) {
-	indent(reporter);
+    indent(reporter);
     printf("<fail>\n");
-	indent(reporter);
-    printf("\t<message><![CDATA[");
+    indent(reporter);
+    printf("\t<message>");
     vprintf(message, arguments);
-    printf("]]></message>\n");
-	indent(reporter);
+    printf("</message>\n");
+    indent(reporter);
     printf("\t<location file=\"%s\" line=\"%d\"/>\n", file, line);
-	indent(reporter);
+    indent(reporter);
     printf("</fail>\n");
 }
 
 static void xml_show_incomplete(TestReporter *reporter, const char *name) {
-	indent(reporter);
+    indent(reporter);
     printf("<fail>\n");
-	indent(reporter);
-    printf("\t<message><![CDATA[Failed to complete]]></message>\n");
-	indent(reporter);
+    indent(reporter);
+    printf("\t<message>Failed to complete]]></message>\n");
+    indent(reporter);
     printf("</fail>\n");
+}
+
+static void xml_reporter_finish_test(TestReporter *reporter, const char *filename, int line) {
+    reporter_finish(reporter, filename, line);
+    indent(reporter);
+    printf("</test>\n");
+}
+
+static void xml_reporter_finish_suite(TestReporter *reporter, const char *filename, int line) {
+    reporter_finish(reporter, filename, line);
+    indent(reporter);
+    printf("</suite>\n");
 }
