1ab63aac194f71f93f761d4c648eb67f57a7f65c
thomasnilsson <thomasnilsson@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2012-10-01 07:13:01 +0000

    Moved CYGWIN option to DefineOptions.cmake Commented out a number of options we don't use Fixed incomplete message in xml reporter

--- trunk/cgreen/cmake/Modules/DefineInstallationPaths.cmake
+++ trunk/cgreen/cmake/Modules/DefineInstallationPaths.cmake
@@ -17,9 +17,10 @@ if (UNIX)
     "${CMAKE_INSTALL_PREFIX}/share"
     CACHE PATH "Base directory for files which go to share/"
   )
-  SET(DATA_INSTALL_PREFIX
-    "${SHARE_INSTALL_PREFIX}/${APPLICATION_NAME}"
-    CACHE PATH "The parent directory where applications can install their data")
+#  SET(DATA_INSTALL_PREFIX
+#    "${SHARE_INSTALL_PREFIX}/${APPLICATION_NAME}"
+#    CACHE PATH "The parent directory where applications can install their data"
+#  )
 
   # The following are directories where stuff will be installed to
   SET(BIN_INSTALL_DIR
@@ -55,33 +56,33 @@ if (UNIX)
     "${DATA_INSTALL_PREFIX}/doc/HTML"
     CACHE PATH "The HTML install dir for documentation (default data/doc/html)"
   )
-  SET(ICON_INSTALL_DIR
-    "${DATA_INSTALL_PREFIX}/icons"
-    CACHE PATH "The icon install dir (default data/icons/)"
-  )
-  SET(SOUND_INSTALL_DIR
-    "${DATA_INSTALL_PREFIX}/sounds"
-    CACHE PATH "The install dir for sound files (default data/sounds)"
-  )
+#  SET(ICON_INSTALL_DIR
+#    "${DATA_INSTALL_PREFIX}/icons"
+#    CACHE PATH "The icon install dir (default data/icons/)"
+#  )
+#  SET(SOUND_INSTALL_DIR
+#    "${DATA_INSTALL_PREFIX}/sounds"
+#    CACHE PATH "The install dir for sound files (default data/sounds)"
+#  )
 
   SET(LOCALE_INSTALL_DIR
     "${SHARE_INSTALL_PREFIX}/locale"
     CACHE PATH "The install dir for translations (default prefix/share/locale)"
   )
 
-  SET(XDG_APPS_DIR
-    "${SHARE_INSTALL_PREFIX}/applications/"
-    CACHE PATH "The XDG apps dir"
-  )
-  SET(XDG_DIRECTORY_DIR
-    "${SHARE_INSTALL_PREFIX}/desktop-directories"
-    CACHE PATH "The XDG directory"
-  )
-
-  SET(SYSCONF_INSTALL_DIR
-    "${EXEC_INSTALL_PREFIX}/etc"
-    CACHE PATH "The ${APPLICATION_NAME} sysconfig install dir (default prefix/etc)"
-  )
+#  SET(XDG_APPS_DIR
+#    "${SHARE_INSTALL_PREFIX}/applications/"
+#    CACHE PATH "The XDG apps dir"
+#  )
+#  SET(XDG_DIRECTORY_DIR
+#    "${SHARE_INSTALL_PREFIX}/desktop-directories"
+#    CACHE PATH "The XDG directory"
+#  )
+#
+#  SET(SYSCONF_INSTALL_DIR
+#    "${EXEC_INSTALL_PREFIX}/etc"
+#    CACHE PATH "The ${APPLICATION_NAME} sysconfig install dir (default prefix/etc)"
+#  )
   SET(MAN_INSTALL_DIR
     "${SHARE_INSTALL_PREFIX}/man"
     CACHE PATH "The ${APPLICATION_NAME} man install dir (default prefix/man)"
@@ -100,8 +101,8 @@ if (WIN32)
   set(INCLUDE_INSTALL_DIR "include" CACHE PATH "-")
   set(PLUGIN_INSTALL_DIR "plugins" CACHE PATH "-")
   set(HTML_INSTALL_DIR "doc/HTML" CACHE PATH "-")
-  set(ICON_INSTALL_DIR "." CACHE PATH "-")
-  set(SOUND_INSTALL_DIR "." CACHE PATH "-")
+#  set(ICON_INSTALL_DIR "." CACHE PATH "-")
+#  set(SOUND_INSTALL_DIR "." CACHE PATH "-")
   set(LOCALE_INSTALL_DIR "lang" CACHE PATH "-")
 endif (WIN32)
 
