409950bfde3d6c712c0ed8d0b897d012548ca9fb
matt_hargett <matt_hargett@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2012-12-07 22:45:51 +0000

    some minor cleanups and notes on a feature request.

--- trunk/cgreen/include/cgreen/cpp_constraint.h
+++ trunk/cgreen/include/cgreen/cpp_constraint.h
@@ -12,4 +12,6 @@ Constraint *create_not_equal_to_string_constraint(const std::string* expected_va
 Constraint *create_contains_string_constraint(const std::string* expected_value, const char *expected_value_name);
 Constraint *create_does_not_contain_string_constraint(const std::string* expected_value, const char *expected_value_name);
 
+// TODO: add create_equal_to_constraint_<T> where operator<< output is used for expected_value name
+
 }
