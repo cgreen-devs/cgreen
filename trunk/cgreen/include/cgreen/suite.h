1d6cce9e082b2f81b66518c59e323da895d754ca
thomasnilsson <thomasnilsson@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2013-01-12 21:36:38 +0000

    Continued separation of public api and internal

--- trunk/cgreen/include/cgreen/suite.h
+++ trunk/cgreen/include/cgreen/suite.h
@@ -12,30 +12,7 @@ namespace cgreen {
     extern "C" {
 #endif
 
-enum {test_function, test_suite};
-
-typedef struct TestSuite_ TestSuite;
-
-typedef struct {
-    int type;
-    const char *name;
-    union {
-        CgreenTest *test;
-        TestSuite *suite;
-    } Runnable;
-} UnitTest;
-
-struct TestSuite_ {
-	const char *name;
-	const char* filename;
-	int line;
-	UnitTest *tests;
-	void (*setup)();
-	void (*teardown)();
-	int size;
-};
-
-void do_nothing(void);
+#include "internal/suite_internal.h"
 
 #define create_test_suite() create_named_test_suite_(__func__, __FILE__, __LINE__)
 #define create_named_test_suite(name) create_named_test_suite_(name, __FILE__, __LINE__)
@@ -44,17 +21,13 @@ void do_nothing(void);
 #define add_tests(suite, ...) add_tests_(suite, #__VA_ARGS__, (CgreenTest *)__VA_ARGS__)
 #define add_suite(owner, suite) add_suite_(owner, #suite, suite)
 
-TestSuite *create_named_test_suite_(const char *name, const char *filename, int line);
-void destroy_test_suite(TestSuite *suite);
-void add_test_(TestSuite *suite, const char *name, CgreenTest *test);
-void add_tests_(TestSuite *suite, const char *names, ...);
-void add_suite_(TestSuite *owner, const char *name, TestSuite *suite);
 void set_setup(TestSuite *suite, void (*set_up)());
 void set_teardown(TestSuite *suite, void (*tear_down)());
 int count_tests(TestSuite *suite);
 bool has_test(TestSuite *suite, const char *name);
 bool has_setup(TestSuite *suite);
 bool has_teardown(TestSuite *suite);
+void destroy_test_suite(TestSuite *suite);
 
 #ifdef __cplusplus
     }
