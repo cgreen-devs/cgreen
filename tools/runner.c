ab271a806aa7352abfb1dae1b5e1dbefa83fa72a
Thomas Nilsson <thoni56@users.noreply.github.com>
2013-03-19 21:10:58 +0000

    Removed cleanup_test_run() from run_test_suite() and run_single_test() to move responsibility of free'ing to caller (and allocator of reporter and suite structures) Added a test case to run with XML-generation to exercise the suite handling and XML-reporting Also went with smart-tabs.el for consistent indentation (http://www.emacswiki.org/SmartTabs)

--- tools/runner.c
+++ tools/runner.c
@@ -110,13 +110,13 @@ static TestSuite *suite_for_context(ContextSuite *suites, const char *context_na
 
 
 /*----------------------------------------------------------------------*/
-static ContextSuite *push_new_context_suite(TestSuite *parent, const char* context_name, ContextSuite *next) {
-	ContextSuite *suite = (ContextSuite *)calloc(1, sizeof(ContextSuite));
-	suite->context = context_name;
-	suite->suite = create_named_test_suite(context_name);
-	suite->next = next;
-	add_suite_(parent, context_name, suite->suite);
-	return suite;
+static ContextSuite *add_new_context_suite(TestSuite *parent, const char* context_name, ContextSuite *next) {
+    ContextSuite *suite = (ContextSuite *)calloc(1, sizeof(ContextSuite));
+    suite->context = context_name;
+    suite->suite = create_named_test_suite(context_name);
+    suite->next = next;
+    add_suite_(parent, context_name, suite->suite);
+    return suite;
 }
 
 
@@ -125,7 +125,7 @@ static void add_test_to_context(TestSuite *parent, ContextSuite **suites, const 
 	TestSuite *test_suite = suite_for_context(*suites, context_name);
 
 	if (test_suite == NULL) {
-		*suites = push_new_context_suite(parent, context_name, *suites);
+		*suites = add_new_context_suite(parent, context_name, *suites);
 		test_suite = (*suites)->suite;
 	}
 	add_test_(test_suite, test_name, test);
@@ -137,18 +137,18 @@ static int add_matching_tests_to_suite(void *handle, const char *symbolic_name_p
 {
 	ContextSuite *context_suites = NULL;
 //	ContextSuite *context_suite;
-    int count = 0;
-//    int i;
+	int count = 0;
+//  int i;
 
-    for (int i = 0; test_items[i].specification_name != NULL; i++) {
-        if (symbolic_name_pattern == NULL || test_matches_pattern(symbolic_name_pattern, test_items[i])) {
-            char *error;
-            CgreenTest *test_function = (CgreenTest *)(dlsym(handle, test_items[i].specification_name));
+	for (int i = 0; test_items[i].specification_name != NULL; i++) {
+		if (symbolic_name_pattern == NULL || test_matches_pattern(symbolic_name_pattern, test_items[i])) {
+			char *error;
+			CgreenTest *test_function = (CgreenTest *)(dlsym(handle, test_items[i].specification_name));
 
-            if ((error = dlerror()) != NULL)  {
-                fprintf (stderr, "%s\n", error);
-                exit(1);
-            }
+			if ((error = dlerror()) != NULL)  {
+				fprintf (stderr, "%s\n", error);
+				exit(1);
+			}
 
             add_test_to_context(suite, &context_suites, test_items[i].context_name, test_items[i].test_name, test_function);
             count++;
