571dc346de7855ee9707c6fca72c70e4146fec90
thomasnilsson <thomasnilsson@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2014-04-12 08:45:59 +0000

    Added a first attempt at a setup script for Cygwin development (since Cygwin is a DLL platform it uses Windows procedure for locating DLLs this bites us every time, the build directories must be a head of any installed versions in the PATH)

---
+++ trunk/cgreen/cygwin-setup.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+# I'm making some notes here for future reference: CYGWIN is *not*
+# WIN32, but it *is* a DLL-platform So it loads DLL:s the same way
+# Windows does, namely from the path. That means that for tests to run
+# using the freshly build DLL:s in the build tree both include paths
+# but also the PATH need to be prepended by the directory of the
+# DLL. This could be done with something like:
+#
+#   set_tests_properties(SomeTest PROPERTIES ENVIRONMENT "PATH=c:\somedir;c:\otherdir")
+#
+# which I found at http://stackoverflow.com/a/1079072/204658. The
+# thing to work out is what CMAKE variables point to that
+# directory. The follwing should be evolved to do just that, so that a
+# CYGWIN developer could just source this script and then forget about
+# it.
+#
+
+# The following is a first (non-successful) attempt at ensuring that
+# the script is sourced:
+if [[ $_ != $0 ]]; then
+   echo "Sourced"
+else
+   echo "Subshell"
+fi
+
+# And here's the meat
+export PATH="$PWD/build/build-c/src":"$PWD/build/build-c++/src":$PATH
+export PATH="$PWD/build/build-c/tools":"$PWD/build/build-c++/tools":$PATH
