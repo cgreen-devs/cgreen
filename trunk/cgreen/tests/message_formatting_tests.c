86f0c36761523093c57ac86d3f5f550aeb2dfb2b
thoni56 <thoni56@20233851-bf8d-403c-a13b-08d1c7d1b0a1>
2014-12-27 23:18:13 +0000

    Fixed a problem with message formatting if strings contained '%' Thanks to Srdjan Veljkovic for reporting

---
+++ trunk/cgreen/tests/message_formatting_tests.c
@@ -0,0 +1,32 @@
+#include <cgreen/cgreen.h>
+#include <cgreen/message_formatting.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+using namespace cgreen;
+#endif
+
+#define compare_constraint(c, x) (*c->compare)(c, (intptr_t)x)
+
+Describe(MessageFormatting);
+BeforeEach(MessageFormatting) {}
+AfterEach(MessageFormatting) {}
+
+
+Ensure(MessageFormatting, can_show_failure_message_containing_percent_sign) {
+    const char *string_with_percent = "This contains %!";
+    Constraint *constraint =
+        create_equal_to_string_constraint(string_with_percent, "string_with_percent");
+
+    char *failure_message = failure_message_for(constraint, "actual_string", (intptr_t)"This contains another %!");
+    assert_that(failure_message, contains_string("contains %%!"));
+    assert_that(failure_message, contains_string("another %%!"));
+
+    destroy_constraint(constraint);
+}
+
+TestSuite *message_formatting_tests() {
+    TestSuite *suite = create_test_suite();
+    add_test_with_context(suite, MessageFormatting, can_show_failure_message_containing_percent_sign);
+    return suite;
+}
