e39217b0161751d124c4b6cf9ae31179264568f4
Thomas Nilsson <thomas@junovagen.se>
2015-09-05 08:47:15 +0100

    Half-way through the mock section

--- doc/tutorial_src/words.h
+++ doc/tutorial_src/words.h
@@ -1,2 +1,2 @@
 int split_words(char *sentence);
-void words(const char *sentence, void (*walker)(const char *, void *), void *);
+void words(const char *sentence, void (*walker)(const char *, void *), void *memo);
