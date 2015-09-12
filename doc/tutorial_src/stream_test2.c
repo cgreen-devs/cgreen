9b1a689701a2da8be406c839ade5114567caec55
Thomas Nilsson <thomas@junovagen.se>
2015-09-12 22:28:30 +0100

    Continued to replace code in tutorial with include of real code

--- doc/tutorial_src/stream_test2.c
+++ doc/tutorial_src/stream_test2.c
@@ -3,34 +3,23 @@
 
 char *read_paragraph(int (*read)(void *), void *stream);
 
-static int stub_stream(void *stream) {
+static int stream_stub(void *stream) {
     return (int)mock(stream);
 }
 
-Ensure(reading_lines_from_empty_stream_gives_null) {
-    always_expect(stub_stream, will_return(EOF));
-    assert_that(read_paragraph(&stub_stream, NULL), is_null);
+Describe(ParagraphReader);
+BeforeEach(ParagraphReader) {}
+AfterEach(ParagraphReader) {}
+
+Ensure(ParagraphReader, gives_null_when_reading_empty_stream) {
+    always_expect(stream_stub, will_return(EOF));                                 // <1>
+    assert_that(read_paragraph(&stream_stub, NULL), is_null);
 }
 
-Ensure(one_character_stream_gives_one_character_line) {
-    expect(stub_stream, will_return('a'));
-    expect(stub_stream, will_return(EOF));
-    char *line = read_paragraph(&stub_stream, NULL);
+Ensure(ParagraphReader, gives_one_character_line_for_one_character_stream) {
+    expect(stream_stub, will_return('a'));
+    expect(stream_stub, will_return(EOF));
+    char *line = read_paragraph(&stream_stub, NULL);
     assert_that(line, is_equal_to_string("a"));
     free(line);
-}
-
-TestSuite *stream_tests() {
-    TestSuite *suite = create_test_suite();
-    add_test(suite, reading_lines_from_empty_stream_gives_null);
-    add_test(suite, one_character_stream_gives_one_character_line);
-    return suite;
-}
-
-int main(int argc, char **argv) {
-    TestSuite *suite = stream_tests();
-    if (argc > 1) {
-        return run_single_test(suite, argv[1], create_text_reporter());
-    }
-    return run_test_suite(suite, create_text_reporter());
 }
