efd0a0520230ea504d5a76f0047066b484c82577
Thomas Nilsson <thomas@junovagen.se>
2015-12-27 17:16:56 +0100

    Moved learning mocks tests to mock_messages_tests. Added the standard error to the output that is compared so that learning mock output is catched and compared.

--- tests/mock_messages_tests.c
+++ tests/mock_messages_tests.c
@@ -16,6 +16,10 @@ static int integer_out() {
     return (int)mock();
 }
 
+static char *string_out(int p1) {
+    return (char *)mock(p1);
+}
+
 static int sample_mock(int i, const char *s) {
     return (int)mock(i, s);
 }
@@ -81,3 +85,30 @@ Ensure(Mocks, single_uncalled_expectation_fails_tally) {
         when(s, is_equal_to_string("devil"))
     );
 }
+
+Ensure(Mocks, learning_mocks_emit_none_when_learning_no_mocks) {
+    cgreen_mocks_are(learning_mocks);
+}
+
+Ensure(Mocks, learning_mocks_emit_pastable_code) {
+    cgreen_mocks_are(learning_mocks);
+    string_out(1);
+    string_out(2);
+    integer_out();
+    integer_out();
+    string_out(3);
+    integer_out();
+}
+
+/* It would be very nice if learning mocks could survive unexpected
+   terminations but since the child process is already gone when the
+   parent process detects this we need to either start catching
+   exceptions in the child or communicate the learned mock calls to
+   the parent some other way.
+
+Ensure(Mocks, learning_mocks_survive_termination) {
+    cgreen_mocks_are(learning_mocks);
+    string_out(1);
+    *(int*)0 = 0;
+}
+*/
