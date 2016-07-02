2ff4720435afb3199202b69143467a0fff1799f4
Thomas Nilsson <thomas@junovagen.se>
2016-07-02 21:26:05 +0200

    Extract legacy assertions to separate include file

--- include/cgreen/assertions.h
+++ include/cgreen/assertions.h
@@ -36,41 +36,7 @@ namespace cgreen {
 int get_significant_figures();
 void significant_figures_for_assert_double_are(int figures);
 
-
-/* Legacy style asserts:*/
-#define assert_true(result) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, result, "[" #result "] should be true\n", NULL)
-#define assert_false(result) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, ! result, "[" #result "] should be false\n", NULL)
-#define assert_equal(tried, expected) \
-        assert_equal_(__FILE__, __LINE__, #tried, (intptr_t)tried, (intptr_t)expected)
-#define assert_not_equal(tried, expected) \
-        assert_not_equal_(__FILE__, __LINE__, #tried, (intptr_t)tried, (intptr_t)expected)
-#define assert_double_equal(tried, expected) \
-        assert_double_equal_(__FILE__, __LINE__, #tried, tried, expected)
-#define assert_double_not_equal(tried, expected) \
-        assert_double_not_equal_(__FILE__, __LINE__, #tried, tried, expected)
-#define assert_string_equal(tried, expected) \
-        assert_string_equal_(__FILE__, __LINE__, #tried, tried, expected)
-#define assert_string_not_equal(tried, expected) \
-        assert_string_not_equal_(__FILE__, __LINE__, #tried, tried, expected)
-
-#define assert_true_with_message(result, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, result, __VA_ARGS__)
-#define assert_false_with_message(result, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, ! result, __VA_ARGS__)
-#define assert_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, (tried == expected), __VA_ARGS__)
-#define assert_not_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, (tried != expected), __VA_ARGS__)
-#define assert_double_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, doubles_are_equal(tried, expected), __VA_ARGS__)
-#define assert_double_not_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, doubles_are_equal(tried, expected), __VA_ARGS__)
-#define assert_string_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, strings_are_equal(tried, expected), __VA_ARGS__)
-#define assert_string_not_equal_with_message(tried, expected, ...) \
-        (*get_test_reporter()->assert_true)(get_test_reporter(), __FILE__, __LINE__, !strings_are_equal(tried, expected), __VA_ARGS__)
+#include <cgreen/legacy.h>
 
 #ifdef __cplusplus
     }
