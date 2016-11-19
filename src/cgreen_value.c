d6578af1710a7ec8961556a626dcd7b568e20e16
Thomas Nilefalk <thomas@junovagen.se>
2016-11-19 20:39:54 +0100

    Exclude 'cgreen_value_tests' from C++ tests

--- src/cgreen_value.c
+++ src/cgreen_value.c
@@ -7,25 +7,25 @@ namespace cgreen {
 #endif
 
     CgreenValue make_cgreen_integer_value(intptr_t integer) {
-        CgreenValue value = {INTEGER, {integer}};
+        CgreenValue value = {INTEGER, {0}};
         value.value.integer_value = integer;
         return value;
     }
 
     CgreenValue make_cgreen_string_value(const char *string) {
-        CgreenValue value = {STRING, {string}};
+        CgreenValue value = {STRING, {0}};
         value.value.string_value = string;
         return value;
     }
     
     CgreenValue make_cgreen_pointer_value(void *pointer) {
-        CgreenValue value = {POINTER, {pointer}};
+        CgreenValue value = {POINTER, {0}};
         value.value.pointer_value = pointer;
         return value;
     }
 
     CgreenValue make_cgreen_double_value(double d) {
-        CgreenValue value = {DOUBLE, {d}};
+        CgreenValue value = {DOUBLE, {0}};
         value.value.double_value = d;
         return value;
     }
