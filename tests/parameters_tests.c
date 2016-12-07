802beefc586a1e68310cc2519c7cf87fb38c27db
Thomas Nilsson <thomas@junovagen.se>
2016-12-07 17:59:37 +0100

    Fix broken parameter extraction for multiple functions ('box_double()' or 'd()', whatever that is). Add tests to verify combination of parameters.

--- tests/parameters_tests.c
+++ tests/parameters_tests.c
@@ -69,9 +69,33 @@ Ensure(can_strip_box_double_to_leave_original_name) {
     assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
 }
 
+Ensure(can_strip_two_box_doubles_to_leave_original_names) {
+    names = create_vector_of_names("box_double(a), box_double(b)");
+    assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
+    assert_that((const char *)cgreen_vector_get(names, 1), is_equal_to_string("b"));
+}
+
 Ensure(can_strip_d_macro_to_leave_original_name) {
     names = create_vector_of_names("d(a)");
     assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
+}
+
+Ensure(can_strip_two_d_macros_to_leave_original_names) {
+    names = create_vector_of_names("d(a), d(b)");
+    assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
+    assert_that((const char *)cgreen_vector_get(names, 1), is_equal_to_string("b"));
+}
+
+Ensure(can_strip_d_macro_and_box_double_to_leave_original_names) {
+    names = create_vector_of_names("d(a), box_double(b)");
+    assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
+    assert_that((const char *)cgreen_vector_get(names, 1), is_equal_to_string("b"));
+}
+
+Ensure(can_strip_box_double_and_d_macro_to_leave_original_names) {
+    names = create_vector_of_names("box_double(a), d(b)");
+    assert_that((const char *)cgreen_vector_get(names, 0), is_equal_to_string("a"));
+    assert_that((const char *)cgreen_vector_get(names, 1), is_equal_to_string("b"));
 }
 
 TestSuite *parameter_tests() {
@@ -86,6 +110,10 @@ TestSuite *parameter_tests() {
     add_test(suite, can_read_long_parameters_with_funky_names);
     add_test(suite, can_read_two_parameters_with_varied_whitespace);
     add_test(suite, can_strip_box_double_to_leave_original_name);
+    add_test(suite, can_strip_two_box_doubles_to_leave_original_names);
     add_test(suite, can_strip_d_macro_to_leave_original_name);
+    add_test(suite, can_strip_two_d_macros_to_leave_original_names);
+    add_test(suite, can_strip_d_macro_and_box_double_to_leave_original_names);
+    add_test(suite, can_strip_box_double_and_d_macro_to_leave_original_names);
     return suite;
 }
