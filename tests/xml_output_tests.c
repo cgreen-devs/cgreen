fd241b7b2d12ebffe3852cd79117fa0383ca7b9a
Thomas Nilefalk <thomas@junovagen.se>
2016-12-17 18:25:16 +0100

    Ensure time information in XML output is normalized

--- tests/xml_output_tests.c
+++ tests/xml_output_tests.c
@@ -1,9 +1,9 @@
 #include <cgreen/cgreen.h>
 
 Ensure(failing_test_is_listed_by_xml_reporter) {
-  assert_that(false);
+    assert_that(false);
 }
 
 Ensure(passing_test_is_listed_by_xml_reporter) {
-  assert_that(true);
+    assert_that(true);
 }
