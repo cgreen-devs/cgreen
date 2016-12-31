0914eaa50474046d1e265870cc569cd9ad2355b0
Thomas Nilefalk <thomas@junovagen.se>
2016-12-31 17:55:13 +0100

    Add function to create markers for mock parameters that are doubles
    The user will need to "mark" all double typed arguments to 'mock()'
    with 'box_double()', which will also happen so that it fit into an
    intptr_t. But we want to know which ones to treat as doubles to create
    and compare with double semantics and type checks.
    
    All part of work initiated by #96

--- src/parameters.c
+++ src/parameters.c
@@ -18,6 +18,7 @@ static char *tokenise_by_commas_and_whitespace(char *list);
 static char *skip_nulls_until(char *pointer, char *pointer_end);
 static char *end_of_token(char *token);
 static char *strip_function_from(char *token, const char *function_name);
+static bool begins_with(char *token, const char *beginning);
 
 static char *stringdup(const char *string) {
     return strcpy((char *)malloc(strlen(string)+1), string);
@@ -52,6 +53,47 @@ CgreenVector *create_vector_of_names(const char *parameters) {
 
     free(tokens);
     return names;
+}
+
+static bool *pointer_to_bool(bool value) {
+    bool *ptr = (bool *)malloc(sizeof(value));
+    *ptr = value;
+    return ptr;
+}
+
+/* TODO: This should probably be folded into the 'create_vector_of_names()' above */
+CgreenVector *create_vector_of_double_markers_for(const char *parameters) {
+    char *parameters_to_tokenize;
+    char *parameters_end;
+    char *tokens;
+    char *token;
+    CgreenVector *markers = create_cgreen_vector(&free);
+    if ((parameters == NULL) || (strlen(parameters) == 0)) {
+        return markers;
+    }
+
+    parameters_to_tokenize = stringdup(parameters);
+    if (parameters_to_tokenize == NULL) {
+        return markers;
+    }
+
+    parameters_end = parameters_to_tokenize + strlen(parameters_to_tokenize);
+    tokens = tokenise_by_commas_and_whitespace(parameters_to_tokenize);
+    token = tokens;
+    while (token < tokens + strlen(parameters)) {
+        token = skip_nulls_until(token, parameters_end);
+        int length_of_token = strlen(token);
+        if (begins_with(token, "box_double"))
+            cgreen_vector_add(markers, pointer_to_bool(true));
+        else
+            cgreen_vector_add(markers, pointer_to_bool(false));
+        token = strip_function_from(token, "box_double");
+        token = strip_function_from(token, "d");
+        token += length_of_token;
+    }
+
+    free(tokens);
+    return markers;
 }
 
 static char *tokenise_by_commas_and_whitespace(char *list) {
@@ -94,7 +136,7 @@ static void move_parameter_to_beginning_of(char *token, const char *function_nam
 static char *strip_function_from(char *token, const char *function_name) {
     if (begins_with(token, function_name) && token[strlen(function_name)] == '(' && last_char_of(token) == ')') {
         int parameter_length = strlen(token) - strlen(function_name) - strlen("()");
-	move_parameter_to_beginning_of(token, function_name, parameter_length);
+        move_parameter_to_beginning_of(token, function_name, parameter_length);
     }
 
     return token;
--- tests/parameters_tests.c
+++ tests/parameters_tests.c
@@ -121,6 +121,33 @@ Ensure(can_strip_multiple_mixed_parameters_to_leave_original_names) {
     assert_that((const char *)cgreen_vector_get(names, 6), is_equal_to_string("g"));
 }
 
+Ensure(can_create_empty_vector_of_double_markers) {
+    CgreenVector *markers = create_vector_of_double_markers_for("");
+    assert_that(cgreen_vector_size(markers), is_equal_to(0));
+}
+
+Ensure(can_create_markers_for_single_non_double) {
+    CgreenVector *markers = create_vector_of_double_markers_for("a");
+    assert_that(cgreen_vector_size(markers), is_equal_to(1));
+    assert_that(!*(bool*)cgreen_vector_get(markers, 0));
+}
+
+Ensure(can_create_markers_for_single_double) {
+    CgreenVector *markers = create_vector_of_double_markers_for("box_double(a)");
+    assert_that(cgreen_vector_size(markers), is_equal_to(1));
+    assert_that(*(bool*)cgreen_vector_get(markers, 0));
+}
+
+Ensure(can_create_markers_for_mixed_parameters) {
+    CgreenVector *markers = create_vector_of_double_markers_for("a, box_double(b), c,d,box_double(e)");
+    assert_that(cgreen_vector_size(markers), is_equal_to(5));
+    assert_that(!*(bool*)cgreen_vector_get(markers, 0));
+    assert_that(*(bool*)cgreen_vector_get(markers, 1));
+    assert_that(!*(bool*)cgreen_vector_get(markers, 2));
+    assert_that(!*(bool*)cgreen_vector_get(markers, 3));
+    assert_that(*(bool*)cgreen_vector_get(markers, 4));
+}
+
 TestSuite *parameter_tests() {
     TestSuite *suite = create_test_suite();
     set_teardown(suite, destroy_names);
