f04556e7286b06beb787b96ec40a57835aa698e3
Thomas Nilefalk <thomas@nilefalk.se>
2017-10-30 07:42:16 +0100

    Add file and line to PANIC messages

--- src/utils.h
+++ src/utils.h
@@ -6,9 +6,10 @@ namespace cgreen {
     extern "C" {
 #endif
 
+#define PANIC(...) panic(__FILE__, __LINE__, __VA_ARGS__)
         char *string_dup(const char *original);
         void panic_set_output_buffer(const char *buffer);
-        void PANIC(const char *fmt, ...);
+        void panic(const char *filename, int line, const char *fmt, ...);
 
 #ifdef __cplusplus
     }
