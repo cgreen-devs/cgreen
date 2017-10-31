050c936740a0237c65d80405a08082ae96bff1e2
Dominic Meiser <dmeiser79@gmail.com>
2017-10-30 20:47:30 -0600

    Fix double comparisons for small numbers
    * Introduce an absolute tolerance.
    * Improve implementation of accuracy function to avoid casting and make the
      logic more transparent.
    * Add discussion of floating point comparisons to manual.
    * Add a test case.
    * Minor cosmetic changes (e.g. renamed a test case).
    * Remove a test case that compares a small number with 0 because this is
      expected behavior.
    
    Discussion
    
    The introduction of an absolute tolerance fixes the underflow issues near 0. If
    two numbers are within the absolute tolerance of one another they are considered
    equal and we don't need to evaluate the problematic accuracy function. We choose
    an absolute tolerance that is 10^8 times larger than the smallest double number
    that can be represented. Eight is the default number of significant digits used
    for relative comparisons. This prevents underflow issues when comparing numbers
    near zero for the default number of significant digits.
    
    Note that our default cannot prevent underflow if the user increases the number
    of significant digits to a value larger than eight. doubles_are_equal will not
    crash in this case but it may wrongly report two numbers as equal. The
    problematic cases are numbers near zero.
    
    We have added a discussion of our floating point comparison algorithm to the
    manual. We point out the problems that can occur when comparing numbers close to
    zero and we propose to use constraints of the form
    
    assert_that_double(fabs(x - y), is_less_than_double(abs_tolerance));
    
    in that case.

--- tests/double_tests.c
+++ tests/double_tests.c
@@ -13,12 +13,12 @@ Ensure(Double, comparison_with_negative_values_should_not_always_succeed) {
     assert_that_double(-500, is_not_equal_to_double(0));
 }
 
-xEnsure(Double, relative_epsilon_calculations_with_0_and_a_very_small_number_should_not_fail) {
-    /* Issue #147 */
-    assert_that_double(0.000000000000001, is_equal_to_double(0));
-}
-
-xEnsure(Double, comparisons_with_extremely_small_numbers_should_not_always_fail) {
+Ensure(Double, comparisons_with_extremely_small_numbers_should_not_always_fail) {
     /*  Issue #149 */
     assert_that_double(4.9406564584124654E-324, is_equal_to_double(4.9406564584124654E-324));
+}
+
+Ensure(Double, numbers_within_absolute_tolerance_of_zero_are_considered_equal_to_zero) {
+    double my_dbl_min = 2.2250738585072014e-308;
+    assert_that_double(1.0e4 * my_dbl_min, is_equal_to_double(0.0));
 }
