62a82c33fdf7dad9085bc6f417b78ff3e08d5027
Thomas Nilefalk <thomas@nilefalk.se>
2017-10-31 21:43:01 +0100

    Read multiple lines and add elements only for tests

--- tools/discoverer.c
+++ tools/discoverer.c
@@ -2,6 +2,31 @@
 
 #include "io.h"
 
+#include <string.h>
+#include <stdbool.h>
+
+#include <cgreen/internal/unit_implementation.h>
+
+
+static bool contains_cgreen_spec(const char *line) {
+    return strstr(line, CGREEN_SPEC_PREFIX CGREEN_SEPARATOR) != NULL;
+}
+
+static bool is_definition(const char *line) {
+    return strstr(line, " D ") != NULL;
+}
+
+static void add_all_tests_from(FILE *nm_output_pipe, CgreenVector *tests) {
+    char line[100];
+    int length = read_line(nm_output_pipe, line, sizeof(line)-1);
+
+    while (length > -1) {
+        if (contains_cgreen_spec(line) && is_definition(line))
+            cgreen_vector_add(tests, NULL); /* TODO Should add test structs */
+        length = read_line(nm_output_pipe, line, sizeof(line)-1);
+    }
+}
+
 CgreenVector *discover_tests_in(const char *filename) {
     FILE *library = open_file(filename, "r");
     if (library == NULL)
@@ -9,13 +34,8 @@ CgreenVector *discover_tests_in(const char *filename) {
     else {
         FILE *nm_output_pipe = open_process("nm test", "r");
         if (nm_output_pipe != NULL) {
-            char buffer[100];
             CgreenVector *tests = create_cgreen_vector(NULL);
-            int length = read_line(nm_output_pipe, buffer, sizeof(buffer)-1);
-            if (length > -1) {
-                cgreen_vector_add(tests, NULL);
-                read_line(nm_output_pipe, buffer, sizeof(buffer)-1);
-            }
+            add_all_tests_from(nm_output_pipe, tests);
             return tests;
         } else
             return NULL;
--- tools/discoverer_tests.c
+++ tools/discoverer_tests.c
@@ -15,23 +15,32 @@ Describe(Discoverer);
 BeforeEach(Discoverer) {}
 AfterEach(Discoverer) {}
 
+static void expect_open_file(const char *filename, void *result) {
+    expect(open_file, when(filename,
+                           is_equal_to_string(filename)),
+           will_return(result));
+}
+
+
 
 Ensure(Discoverer, should_find_no_tests_in_non_existing_file) {
-    expect(open_file, when(filename,
-                           is_equal_to_string("non-existing-file")),
-           will_return(NULL));
+    expect_open_file("non-existing-file", NULL);
 
     CgreenVector *tests = discover_tests_in("non-existing-file");
 
     assert_that(tests, is_null);
 }
 
+
+static void expect_open_process(const char *partial_command, void *result) {
+    expect(open_process, when(command, contains_string(partial_command)),
+           will_return(result));
+}
+
+
 Ensure(Discoverer, should_find_no_tests_in_existing_empty_file) {
-    expect(open_file, when(filename,
-                           is_equal_to_string("empty-file")),
-           will_return(1));
-    expect(open_process, when(command, contains_string("nm ")),
-           will_return(2));
+    expect_open_file("empty-file", (void *)1);
+    expect_open_process("nm ", (void *)2);
     expect(read_line, when(file, is_equal_to(2)),
            will_return(EOF));     /* End of input */
 
@@ -40,21 +49,65 @@ Ensure(Discoverer, should_find_no_tests_in_existing_empty_file) {
     assert_that(cgreen_vector_size(tests), is_equal_to(0));
 }
 
-Ensure(Discoverer, should_find_one_test_in_file_with_one_line_containing_testname_pattern) {
-    char first_line[] = "alfjslfjlsdjkflj";
+static void expect_read_line_from(int file_id, char line[]) {
+    expect(read_line, when(file, is_equal_to(file_id)),
+           will_set_contents_of_parameter(buffer, line, strlen(line)+1),
+           will_return(strlen(line)>0?strlen(line)+1:EOF));
+}
 
-    expect(open_file, when(filename,
-                           is_equal_to_string("some-file")),
-           will_return(1));
-    expect(open_process, when(command, contains_string("nm ")),
-           will_return(2));
-    expect(read_line, when(file, is_equal_to(2)),
-           will_set_contents_of_parameter(buffer, first_line, sizeof(first_line)),
-           will_return(sizeof(first_line)));
-    expect(read_line, when(file, is_equal_to(2)),
-           will_return(EOF));
+
+Ensure(Discoverer, should_find_one_test_in_file_with_one_line_containing_testname_pattern) {
+    char line[] = "0000000000202160 D CgreenSpec__Discoverer__should_find_no_tests_in_existing_empty_file__";
+
+    expect_open_file("some-file", (void *)1);
+    expect_open_process("nm ", (void *)2);
+    expect_read_line_from(2, line);
+    expect_read_line_from(2, "");
 
     CgreenVector *tests = discover_tests_in("some-file");
 
     assert_that(cgreen_vector_size(tests), is_equal_to(1));
+}
+
+Ensure(Discoverer, should_find_two_test_in_two_line_file_with_two_lines_containing_testname_pattern) {
+    char line1[] = "0000000000202160 D CgreenSpec__Discoverer__test1__";
+    char line2[] = "0000000000202160 D CgreenSpec__Discoverer__test2__";
+
+    expect_open_file("some-file", (void *)1);
+    expect_open_process("nm ", (void *)2);
+    expect_read_line_from(2, line1);
+    expect_read_line_from(2, line2);
+    expect_read_line_from(2, "");
+
+    CgreenVector *tests = discover_tests_in("some-file");
+
+    assert_that(cgreen_vector_size(tests), is_equal_to(2));
+}
+
+Ensure(Discoverer, should_find_one_test_in_two_line_file_with_one_line_containing_testname_pattern) {
+    char line1[] = "0000000000202160 D CgreenSpec__Discoverer__test1__";
+    char line2[] = "0000000000202160 D ID";
+
+    expect_open_file("some-file", (void *)1);
+    expect_open_process("nm ", (void *)2);
+    expect_read_line_from(2, line1);
+    expect_read_line_from(2, line2);
+    expect_read_line_from(2, "");
+
+    CgreenVector *tests = discover_tests_in("some-file");
+
+    assert_that(cgreen_vector_size(tests), is_equal_to(1));
+}
+
+Ensure(Discoverer, should_find_no_test_in_file_with_no_definiton) {
+    char line[] = "0000000000202160 U CgreenSpec__Discoverer__test1__";
+
+    expect_open_file("some-file", (void *)1);
+    expect_open_process("nm ", (void *)2);
+    expect_read_line_from(2, line);
+    expect_read_line_from(2, "");
+
+    CgreenVector *tests = discover_tests_in("some-file");
+
+    assert_that(cgreen_vector_size(tests), is_equal_to(0));
 }
