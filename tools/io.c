50bfb07aa2dd05a9998cb0776eb8689a4dbcea3c
Thomas Nilefalk <thomas@nilefalk.se>
2017-11-05 23:14:56 +0100

    Introduce close_file() and close_process()

--- tools/io.c
+++ tools/io.c
@@ -9,8 +9,16 @@ FILE *open_file(const char *filename, const char *mode) {
     return fopen(filename, mode);
 }
 
+int close_file(FILE *file) {
+    return fclose(file);
+}
+
 FILE *open_process(const char *command, const char *mode) {
     return popen(command, mode);
+}
+
+int close_process(FILE *process) {
+    return pclose(process);
 }
 
 
@@ -22,5 +30,5 @@ FILE *open_process(const char *command, const char *mode) {
 */
 int read_line(FILE *file, char *buffer, int max_length) {
     char *result = fgets(buffer, max_length, file);
-    return result==NULL? EOF : strlen(result);
+    return result==NULL? EOF : (signed)strlen(result);
 }
--- tools/io.h
+++ tools/io.h
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 extern FILE *open_file(const char *filename, const char *mode);
+extern int close_file(FILE *file);
+
 extern FILE *open_process(const char *command, const char *mode);
+extern int close_process(FILE *file);
+
 extern int read_line(FILE *file, char *buffer, int max_length);
