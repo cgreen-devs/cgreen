c76f6a20150972314f2f71288d1f02cf4fca704b
Thomas Nilefalk <thomas@nilefalk.se>
2017-11-29 23:00:53 +0100

    Minor formatting fixes

--- include/cgreen/internal/mocks_internal.h
+++ include/cgreen/internal/mocks_internal.h
@@ -1,5 +1,5 @@
-#ifndef MOCKS_INTERNAL_HEADER
-#define MOCKS_INTERNAL_HEADER
+#ifndef MOCKS_INTERNAL_H
+#define MOCKS_INTERNAL_H
 
 #include <cgreen/internal/mock_table.h>
 #include <cgreen/internal/function_macro.h>
@@ -20,10 +20,13 @@ namespace cgreen {
     extern "C" {
 #endif
 
-void expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line, ...);
-void always_expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line, ...);
-void never_expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line, ...);
-intptr_t mock_(TestReporter *test_reporter, const char *function, const char *mock_file, int mock_line, const char *parameters, ...);
+extern void expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line, ...);
+extern void always_expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line,
+                           ...);
+extern void never_expect_(TestReporter *test_reporter, const char *function, const char *test_file, int test_line,
+                          ...);
+extern intptr_t mock_(TestReporter *test_reporter, const char *function, const char *mock_file, int mock_line,
+                      const char *parameters, ...);
 
 
 /* Warning for unused results from 'when()' helps detecting slip-ups in the expect():
@@ -37,10 +40,10 @@ intptr_t mock_(TestReporter *test_reporter, const char *function, const char *mo
    passes the test case. /@awilke
 
 */
-Constraint *when_(const char *parameter, Constraint *constraint) WARN_UNUSED_RESULT;
+extern Constraint *when_(const char *parameter, Constraint *constraint) WARN_UNUSED_RESULT;
 
-void clear_mocks(void);
-void tally_mocks(TestReporter *reporter);
+extern void clear_mocks(void);
+extern void tally_mocks(TestReporter *reporter);
 
 #ifdef __cplusplus
     }
