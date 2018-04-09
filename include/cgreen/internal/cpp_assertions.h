d4a69f5fd93ec6250d8c7e89fa6cc3800db547cd
Thomas Nilefalk <thomas@nilefalk.se>
2018-04-09 17:42:40 +0200

    Exchange tabs for space

--- include/cgreen/internal/cpp_assertions.h
+++ include/cgreen/internal/cpp_assertions.h
@@ -10,33 +10,33 @@
 
 namespace cgreen {
 
-    #define assert_that_constraint(actual, constraint) assert_that_(__FILE__, __LINE__, STRINGIFY_TOKEN(actual), actual, constraint)
+#define assert_that_constraint(actual, constraint) assert_that_(__FILE__, __LINE__, STRINGIFY_TOKEN(actual), actual, constraint)
 
-	void assert_that_(const char *file, int line, const char *actual_string, const std::string& actual, Constraint *constraint);
-	void assert_that_(const char *file, int line, const char *actual_string, const std::string *actual, Constraint *constraint);
-	void assert_that_(const char *file, int line, const char *actual_string, double actual, Constraint *constraint);
+    void assert_that_(const char *file, int line, const char *actual_string, const std::string& actual, Constraint *constraint);
+    void assert_that_(const char *file, int line, const char *actual_string, const std::string *actual, Constraint *constraint);
+    void assert_that_(const char *file, int line, const char *actual_string, double actual, Constraint *constraint);
 
-	// this isn't declared in assertions.h because you can't have overloads for an extern "C"-declared function, so it seems
-	void assert_that_(const char *file, int line, const char *actual_string, intptr_t actual, Constraint *constraint);
+    // this isn't declared in assertions.h because you can't have overloads for an extern "C"-declared function, so it seems
+    void assert_that_(const char *file, int line, const char *actual_string, intptr_t actual, Constraint *constraint);
 
-	template <typename T> void assert_that_(const char *file, int line, const char *actual_string, T actual, Constraint *constraint) {
+    template <typename T> void assert_that_(const char *file, int line, const char *actual_string, T actual, Constraint *constraint) {
 
-		if (typeid(actual) == typeid(std::string&) ||
-			typeid(actual) == typeid(const std::string&) ||
-			typeid(actual) == typeid(const std::string*) ||
-			typeid(actual) == typeid(std::string*)) {
+        if (typeid(actual) == typeid(std::string&) ||
+            typeid(actual) == typeid(const std::string&) ||
+            typeid(actual) == typeid(const std::string*) ||
+            typeid(actual) == typeid(std::string*)) {
 
-			assert_that_(file, line, actual_string, reinterpret_cast<const std::string *>(actual), constraint);
+            assert_that_(file, line, actual_string, reinterpret_cast<const std::string *>(actual), constraint);
 
-		} else if (typeid(actual) == typeid(std::string)) {
+        } else if (typeid(actual) == typeid(std::string)) {
 
-			assert_that_(file, line, actual_string, (const std::string *)&actual, constraint);
+            assert_that_(file, line, actual_string, (const std::string *)&actual, constraint);
 
-		} else {
-			// TODO: update actual_string with output from operator<< of (T)actual
-			assert_that_(file, line, actual_string, (intptr_t)actual, constraint);
-		}
-	}
+        } else {
+            // TODO: update actual_string with output from operator<< of (T)actual
+            assert_that_(file, line, actual_string, (intptr_t)actual, constraint);
+        }
+    }
 }
 
 #endif
