c87e3f3fdadc37cb3d0c47dfefcc6822b0d4b987
Thomas Nilefalk <thomas@nilefalk.se>
2018-05-10 10:23:38 +0200

    Minor formatting

--- tools/discoverer.c
+++ tools/discoverer.c
@@ -45,7 +45,8 @@ static void add_all_tests_from(FILE *nm_output_pipe, CgreenVector *tests, bool v
             strip_newline_from(line);
             TestItem *test_item = create_test_item_from(cgreen_spec_start_of(line));
             if (verbose)
-                printf("Discovered %s:%s (%s)\n", test_item->context_name, test_item->test_name, test_item->specification_name);
+                printf("Discovered %s:%s (%s)\n", test_item->context_name, test_item->test_name,
+                       test_item->specification_name);
             cgreen_vector_add(tests, test_item);
         }
         length = read_line(nm_output_pipe, line, sizeof(line)-1);
