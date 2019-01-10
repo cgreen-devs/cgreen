1f7e39bf244c5e940e6650c96256d2dd3d0a3161
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-01-10 17:01:10 +0100

    Added a unit test for 'show_fail' of cute_reporter
    This new unit tests checks that the arguments which are given to build
    the failure_message are actually used (now that I removed the buffer).

--- tests/cute_reporter_tests.c
+++ tests/cute_reporter_tests.c
@@ -33,15 +33,24 @@ static char *concat(char *output, char *buffer) {
     return output;
 }
 
-static int mocked_printf(const char *format, ...) {
+static int mocked_vprintf(const char *format, va_list arguments) {
     char buffer[10000];
-    va_list ap;
-    va_start(ap, format);
-    vsprintf(buffer, format, ap);
-    va_end(ap);
+
+    vsprintf(buffer, format, arguments);
 
     output = concat(output, buffer);
     return strlen(output);
+}
+
+static int mocked_printf(const char *format, ...) {
+   int result = 0;
+
+   va_list ap;
+   va_start(ap, format);
+   result = mocked_vprintf(format, ap);
+   va_end(ap);
+
+   return result;
 }
 
 static TestReporter *reporter;
@@ -56,6 +65,7 @@ static void setup_cute_reporter_tests(void) {
 
     clear_output();
     set_cute_reporter_printer(reporter, mocked_printf);
+    set_cute_reporter_vprinter(reporter, mocked_vprintf);
 }
 
 static void teardown_cute_reporter_tests(void) {
@@ -124,6 +134,31 @@ Ensure(CuteReporter, will_report_failing_of_test_only_once) {
     reporter->failures++;   // Simulating another failed assert
     reporter->show_fail(reporter, "file", 2, "test_name", arguments);
     assert_no_output();
+
+    // Must indicate test case completion before calling finish_test()
+    send_reporter_completion_notification(reporter);
+    reporter->finish_test(reporter, "filename", line, NULL);
+    assert_no_output();
+}
+
+static void wrapped_show_fail(TestReporter *reporter, const char *file, int line,
+        const char *message, ...) {
+   va_list arguments;
+   va_start(arguments, message);
+   reporter->show_fail(reporter, file, line, message, arguments);
+   va_end(arguments);
+}
+
+Ensure(CuteReporter, will_use_arguments_for_show_fail) {
+    reporter->start_test(reporter, "test_name");
+
+    clear_output();
+    reporter->failures++;   // Simulating a failed assert
+    wrapped_show_fail(reporter, "file", 2, "test_case %i provides %s", 42, "the answer");
+    assert_that(output, begins_with_string("#failure"));
+    assert_that(output, contains_string("test_case 42 provides the answer"));
+
+    clear_output();
 
     // Must indicate test case completion before calling finish_test()
     send_reporter_completion_notification(reporter);
