8c48c26af9e2adbbeec8f823db54790d5646d35a
Hannes Lerchl <hannes.lerchl@googlemail.com>
2019-01-11 08:03:01 +0100

    Fixed potential overflow in cdash_reporter
    As before with cute_reporter and test_reporter, I removed the buffering
    mechanism from cdash_reporter. This fixes a potential buffer overrun for
    huge messages. Also added a unit test to show that the given arguments
    are used by show_fail to build the output.

--- src/cdash_reporter_internal.h
+++ src/cdash_reporter_internal.h
@@ -1,12 +1,16 @@
 #ifndef CDASH_REPORTER_INTERNAL_H
 #define CDASH_REPORTER_INTERNAL_H
 
+#include <stdarg.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef int CDashPrinter(FILE *, const char *format, ...);
+typedef int CDashVPrinter(FILE *, const char *format, va_list arguments);
 void set_cdash_reporter_printer(TestReporter *reporter, CDashPrinter *printer);
+void set_cdash_reporter_vprinter(TestReporter *reporter, CDashVPrinter *vprinter);
 
 #ifdef __cplusplus
 }
