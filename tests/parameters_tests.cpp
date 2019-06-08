a96be463285a9242e5d18cacb4e9ddefcd637b8a
Thomas Nilefalk <thomas@nilefalk.se>
2019-06-08 11:46:21 +0200

    Change all symbolic links to #include's to avoid MSYS2 problems

--- tests/parameters_tests.cpp
+++ tests/parameters_tests.cpp
@@ -1,1 +1,12 @@
-parameters_tests.c
+/*
+  This file used to be a link to the corresponding .c file because we
+  want to compile the same tests for C and C++. But since some systems
+  don't handle symbolic links the same way as *ix systems we get
+  inconsistencies (looking at you Cygwin) or plain out wrong (looking
+  at you MSYS2, copying ?!?!?) behaviour.
+
+  So we will simply include the complete .c source instead...
+ */
+
+#include "parameters_tests.c"
+
