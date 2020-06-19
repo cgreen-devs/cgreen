ff54fc2c63be349e1058df343a65b0cb59c8ce0b
Thomas Nilefalk <thomas@nilefalk.se>
2020-06-19 10:25:52 +0200

    Introduce separate completion for cgreen-debug (fixes #214)

--- contrib/completion/cgreen_completion.bash
+++ contrib/completion/cgreen_completion.bash
@@ -49,5 +49,48 @@ _cgreen_runner_completion()
     COMPREPLY=($(compgen -W '$(printf "%s " ${options[@]} ${libraries[@]} ${tests[@]})' -- "$completion_word"))
 }
 
+_cgreen_debug_completion()
+{
+    local libraries tests
+    libraries=( *.so )
+    tests=()
+
+    # Look for words in the command given so far
+    for word in ${COMP_WORDS[@]}; do
+        if echo $word | grep -q -E "\b\.so\b"; then
+            # If it was a library, check for tests in it
+            if test ! -f  $word || test ! -x $word; then continue; fi
+
+            local SUT="$(nm -f posix $word | grep -o -E 'CgreenSpec\w*?\b' | awk -F '__' '{ print $2 }' | uniq)"
+            local test_names=( $(nm -f posix $word | grep -o -E 'CgreenSpec\w*?\b' | sed -e 's/CgreenSpec__[a-zA-Z0-9]\+\?__//' -e 's/__$//') )
+
+            if test $SUT = "default" ; then
+                tests+=( ${test_names[@]} )
+            else
+                local prefix="$SUT\\:"
+                tests+=( ${test_names[@]/#/$prefix} )
+            fi
+        fi
+    done
+
+    # Remove libraries and tests if already used (only one library and one test allowed)
+    for word in ${COMP_WORDS[@]}; do
+        if [[ $word == *".so" ]]; then
+            # Only one library allowed
+            libraries=()
+        fi
+        if [[ $word == *"\\:"* ]]; then
+            if [[ "${COMP_WORDS[$COMP_CWORD]}" == "" ]]; then
+                # Only one test allowed
+                tests=()
+            fi
+        fi
+    done
+
+    # Need to double the backslashes in the completion word for them to survive into matching
+    completion_word=${COMP_WORDS[$COMP_CWORD]/\\/\\\\}
+    COMPREPLY=($(compgen -W '$(printf "%s " ${options[@]} ${libraries[@]} ${tests[@]})' -- "$completion_word"))
+}
+
 complete -o nosort -o dirnames -F _cgreen_runner_completion cgreen-runner
-complete -o nosort -o dirnames -F _cgreen_runner_completion cgreen-debug
+complete -o nosort -F _cgreen_debug_completion cgreen-debug
