5f6c09b0f8b859f464dd3c5cabaf45851bd1a43f
Thomas Nilefalk <thomas@nilefalk.se>
2020-07-04 11:11:02 +0200

    Remove now empty constraint_syntax_helpers.c

--- src/constraint_syntax_helpers.c
+++
@@ -1,9 +0,0 @@
-#include <cgreen/constraint_syntax_helpers.h>
-#include <cgreen/constraint.h>
-#include "constraint_internal.h"
-#include <cgreen/message_formatting.h>
-#include <stdbool.h>
-#include <stddef.h>
-
-
-/* vim: set ts=4 sw=4 et cindent: */
