bc7822076724b09607f6bc0e0d65f2deb061fcc3
Antonia Siegert <a.siegert@anselko.de>
2021-05-11 13:57:56 +0200

    Fix #256 XML test reporter does not escape chars.
    This fixes the issue, that the XML test reporter produces invalid
    XML when string contraints are used and/or test strings contain
    characters which have to be escaped: <, >, &, ' or "
    
    Chars in fail messages are now getting replaced with the proper escape
    sequences "&lt;", "&gt;", "&amp;", "&apos;" or "&quot;", resp.

--- tests/xml_output_tests.c
+++ tests/xml_output_tests.c
@@ -1,4 +1,5 @@
 #include <cgreen/cgreen.h>
+#include <cgreen/constraint_syntax_helpers.h>
 
 Ensure(failing_test_is_listed_by_xml_reporter) {
     assert_that(false);
@@ -6,4 +7,15 @@ Ensure(failing_test_is_listed_by_xml_reporter) {
 
 Ensure(passing_test_is_listed_by_xml_reporter) {
     assert_that(true);
+}
+
+Ensure(error_message_gets_escaped_by_xml_reporter) {
+    char *test_string =
+        "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n"
+        "<example name=\"arbitrary xml content\">\n"
+        "    <content>I'm the content & have chars which have to be escaped, "
+        "if put in outer XML.</content>\n"
+        "</example>";
+    char *expected_string = "I'm not to be found!";
+    assert_that(test_string, contains_string(expected_string));
 }
