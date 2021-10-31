9fc6c520095d7f8d9a79579c85ca05a9e1ed54b3
Thomas Nilefalk <thomas@nilefalk.se>
2021-10-31 13:50:39 +0100

    [code][format] Untabify some code

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -27,36 +27,36 @@ AfterEach(Unittests) {
 }
 
 Ensure(Unittests, count_tests_return_zero_for_empty_suite) {
-	assert_that(count_tests(suite), is_equal_to(0));
+    assert_that(count_tests(suite), is_equal_to(0));
 }
 
 Ensure(Unittests, count_tests_return_one_for_suite_with_one_testcase) {
-	add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	assert_that(count_tests(suite), is_equal_to(1));
+    add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    assert_that(count_tests(suite), is_equal_to(1));
 }
 
 Ensure(Unittests, count_tests_return_four_for_four_nested_suite_with_one_testcase_each) {
-	TestSuite *suite2 = create_test_suite();
-	TestSuite *suite3 = create_test_suite();
-	TestSuite *suite4 = create_test_suite();
-	add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	add_suite(suite, suite2);
-	add_test_with_context(suite2, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	add_suite(suite2, suite3);
-	add_test_with_context(suite3, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	add_suite(suite3, suite4);
-	add_test_with_context(suite4, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	assert_that(count_tests(suite), is_equal_to(4));
+    TestSuite *suite2 = create_test_suite();
+    TestSuite *suite3 = create_test_suite();
+    TestSuite *suite4 = create_test_suite();
+    add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    add_suite(suite, suite2);
+    add_test_with_context(suite2, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    add_suite(suite2, suite3);
+    add_test_with_context(suite3, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    add_suite(suite3, suite4);
+    add_test_with_context(suite4, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    assert_that(count_tests(suite), is_equal_to(4));
 }
 
 TestSuite *unit_tests(void) {
-	TestSuite *suite = create_test_suite();
-	set_setup(suite, unit_tests_setup);
+    TestSuite *suite = create_test_suite();
+    set_setup(suite, unit_tests_setup);
 
-	add_test_with_context(suite, Unittests, count_tests_return_zero_for_empty_suite);
-	add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
-	add_test_with_context(suite, Unittests, count_tests_return_four_for_four_nested_suite_with_one_testcase_each);
+    add_test_with_context(suite, Unittests, count_tests_return_zero_for_empty_suite);
+    add_test_with_context(suite, Unittests, count_tests_return_one_for_suite_with_one_testcase);
+    add_test_with_context(suite, Unittests, count_tests_return_four_for_four_nested_suite_with_one_testcase_each);
 
-	set_teardown(suite, unit_tests_teardown);
-	return suite;
+    set_teardown(suite, unit_tests_teardown);
+    return suite;
 }
