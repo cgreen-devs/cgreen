a61c04d611bb38a563be405a5e43c1a073fc13cd
Thomas Nilefalk <thomas@nilefalk.se>
2025-08-31 13:47:11 +0200

    [cpp] Add __CGREEN_NAMESPACE_PREFIX (empty for C) to all constraint macros

--- include/cgreen/constraint_syntax_helpers.h
+++ include/cgreen/constraint_syntax_helpers.h
@@ -17,81 +17,48 @@
  */
 
 #ifdef __cplusplus
-#define is_equal_to(value) cgreen::create_equal_to_value_constraint((intptr_t)value, #value)
-#define is_equal_to_hex(value) cgreen::create_equal_to_hexvalue_constraint((intptr_t)value, #value)
-#define is_not_equal_to(value) cgreen::create_not_equal_to_value_constraint((intptr_t)value, #value)
-#define is_non_null is_not_null
-#define is_not_null cgreen::create_not_null_constraint()
-#define is_null cgreen::create_is_null_constraint()
-#define is_false cgreen::create_is_false_constraint()
-#define is_true cgreen::create_is_true_constraint()
-
-#define is_greater_than(value) cgreen::create_greater_than_value_constraint((intptr_t)value, #value)
-#define is_less_than(value) cgreen::create_less_than_value_constraint((intptr_t)value, #value)
-
-#define is_equal_to_contents_of(pointer, size_of_contents) cgreen::create_equal_to_contents_constraint((void *)pointer, size_of_contents, #pointer)
-#define is_not_equal_to_contents_of(pointer, size_of_contents) cgreen::create_not_equal_to_contents_constraint((void *)pointer, size_of_contents, #pointer)
-
-#define is_equal_to_string(value) cgreen::create_equal_to_string_constraint(value, #value)
-#define is_not_equal_to_string(value) cgreen::create_not_equal_to_string_constraint(value, #value)
-#define contains_string(value) cgreen::create_contains_string_constraint(value, #value)
-#define does_not_contain_string(value) cgreen::create_does_not_contain_string_constraint(value, #value)
-#define begins_with_string(value) cgreen::create_begins_with_string_constraint(value, #value)
-#define does_not_begin_with_string(value) cgreen::create_does_not_begin_with_string_constraint(value, #value)
-#define ends_with_string(value) cgreen::create_ends_with_string_constraint(value, #value)
-#define does_not_end_with_string(value) cgreen::create_does_not_end_with_string_constraint(value, #value)
-
-#define is_equal_to_double(value) cgreen::create_equal_to_double_constraint(value, #value)
-#define is_not_equal_to_double(value) cgreen::create_not_equal_to_double_constraint(value, #value)
-
-#define is_less_than_double(value) cgreen::create_less_than_double_constraint(value, #value)
-#define is_greater_than_double(value) cgreen::create_greater_than_double_constraint(value, #value)
-
-
-#define with_side_effect(callback, data) cgreen::create_with_side_effect_constraint(callback, data)
-#define will_return(value) cgreen::create_return_value_constraint((intptr_t)value)
-#define will_return_by_value(value, size) cgreen::create_return_by_value_constraint((intptr_t)&value, size)
-#define will_return_double(value) cgreen::create_return_double_value_constraint(value)
-#define will_set_contents_of_parameter(parameter_name, pointer_to_value, size) cgreen::create_set_parameter_value_constraint(#parameter_name, (intptr_t)pointer_to_value, (size_t)size)
-#define will_capture_parameter(parameter_name, local_variable) cgreen::create_capture_parameter_constraint(#parameter_name, &local_variable, sizeof(local_variable))
+#define __CGREEN_NAMESPACE_PREFIX cgreen::
 #else
-#define is_equal_to(value) create_equal_to_value_constraint((intptr_t)(value), #value)
-#define is_equal_to_hex(value) create_equal_to_hexvalue_constraint((intptr_t)(value), #value)
-#define is_not_equal_to(value) create_not_equal_to_value_constraint((intptr_t)(value), #value)
+#define __CGREEN_NAMESPACE_PREFIX /* empty */
+#endif
+
+#define is_equal_to(value) __CGREEN_NAMESPACE_PREFIX create_equal_to_value_constraint((intptr_t)(value), #value)
+#define is_equal_to_hex(value) __CGREEN_NAMESPACE_PREFIX create_equal_to_hexvalue_constraint((intptr_t)(value), #value)
+#define is_not_equal_to(value) __CGREEN_NAMESPACE_PREFIX create_not_equal_to_value_constraint((intptr_t)(value), #value)
 #define is_non_null is_not_null
-#define is_not_null create_not_null_constraint()
-#define is_null create_is_null_constraint()
-#define is_false create_is_false_constraint()
-#define is_true create_is_true_constraint()
+#define is_not_null __CGREEN_NAMESPACE_PREFIX create_not_null_constraint()
+#define is_null __CGREEN_NAMESPACE_PREFIX create_is_null_constraint()
+#define is_false __CGREEN_NAMESPACE_PREFIX create_is_false_constraint()
+#define is_true __CGREEN_NAMESPACE_PREFIX create_is_true_constraint()
 
-#define is_greater_than(value) create_greater_than_value_constraint((intptr_t)(value), #value)
-#define is_less_than(value) create_less_than_value_constraint((intptr_t)(value), #value)
+#define is_greater_than(value) __CGREEN_NAMESPACE_PREFIX create_greater_than_value_constraint((intptr_t)(value), #value)
+#define is_less_than(value) __CGREEN_NAMESPACE_PREFIX create_less_than_value_constraint((intptr_t)(value), #value)
 
-#define is_equal_to_contents_of(pointer, size_of_contents) create_equal_to_contents_constraint((void *)(pointer), size_of_contents, #pointer)
-#define is_not_equal_to_contents_of(pointer, size_of_contents) create_not_equal_to_contents_constraint((void *)(pointer), size_of_contents, #pointer)
+#define is_equal_to_contents_of(pointer, size_of_contents) __CGREEN_NAMESPACE_PREFIX create_equal_to_contents_constraint((void *)(pointer), size_of_contents, #pointer)
+#define is_not_equal_to_contents_of(pointer, size_of_contents) __CGREEN_NAMESPACE_PREFIX create_not_equal_to_contents_constraint((void *)(pointer), size_of_contents, #pointer)
 
-#define is_equal_to_string(value) create_equal_to_string_constraint(value, #value)
-#define is_not_equal_to_string(value) create_not_equal_to_string_constraint(value, #value)
-#define contains_string(value) create_contains_string_constraint(value, #value)
-#define does_not_contain_string(value) create_does_not_contain_string_constraint(value, #value)
-#define begins_with_string(value) create_begins_with_string_constraint(value, #value)
-#define does_not_begin_with_string(value) create_does_not_begin_with_string_constraint(value, #value)
-#define ends_with_string(value) create_ends_with_string_constraint(value, #value)
-#define does_not_end_with_string(value) create_does_not_end_with_string_constraint(value, #value)
+#define is_equal_to_string(value) __CGREEN_NAMESPACE_PREFIX create_equal_to_string_constraint(value, #value)
+#define is_not_equal_to_string(value) __CGREEN_NAMESPACE_PREFIX create_not_equal_to_string_constraint(value, #value)
+#define contains_string(value) __CGREEN_NAMESPACE_PREFIX create_contains_string_constraint(value, #value)
+#define does_not_contain_string(value) __CGREEN_NAMESPACE_PREFIX create_does_not_contain_string_constraint(value, #value)
+#define begins_with_string(value) __CGREEN_NAMESPACE_PREFIX create_begins_with_string_constraint(value, #value)
+#define does_not_begin_with_string(value) __CGREEN_NAMESPACE_PREFIX create_does_not_begin_with_string_constraint(value, #value)
+#define ends_with_string(value) __CGREEN_NAMESPACE_PREFIX create_ends_with_string_constraint(value, #value)
+#define does_not_end_with_string(value) __CGREEN_NAMESPACE_PREFIX create_does_not_end_with_string_constraint(value, #value)
 
-#define is_equal_to_double(value) create_equal_to_double_constraint(value, #value)
-#define is_not_equal_to_double(value) create_not_equal_to_double_constraint(value, #value)
+#define is_equal_to_double(value) __CGREEN_NAMESPACE_PREFIX create_equal_to_double_constraint(value, #value)
+#define is_not_equal_to_double(value) __CGREEN_NAMESPACE_PREFIX create_not_equal_to_double_constraint(value, #value)
 
-#define is_less_than_double(value) create_less_than_double_constraint(value, #value)
-#define is_greater_than_double(value) create_greater_than_double_constraint(value, #value)
+#define is_less_than_double(value) __CGREEN_NAMESPACE_PREFIX create_less_than_double_constraint(value, #value)
+#define is_greater_than_double(value) __CGREEN_NAMESPACE_PREFIX create_greater_than_double_constraint(value, #value)
 
 
-#define with_side_effect(callback, data) create_with_side_effect_constraint(callback, data)
-#define will_return(value) create_return_value_constraint((intptr_t)(value))
-#define will_return_by_value(value, size) create_return_by_value_constraint((intptr_t)&(value), size)
-#define will_return_double(value) create_return_double_value_constraint(value)
-#define will_set_contents_of_parameter(parameter_name, pointer_to_value, size) create_set_parameter_value_constraint(#parameter_name, (intptr_t)(pointer_to_value), (size_t)(size))
-#define will_set_contents_of_output_parameter(parameter_name, pointer_to_value, size) create_set_parameter_value_constraint(#parameter_name, (intptr_t)(pointer_to_value), (size_t)(size))
-#define will_capture_parameter(parameter_name, local_variable) create_capture_parameter_constraint(#parameter_name, &(local_variable), sizeof(local_variable))
+#define with_side_effect(callback, data) __CGREEN_NAMESPACE_PREFIX create_with_side_effect_constraint(callback, data)
+#define will_return(value) __CGREEN_NAMESPACE_PREFIX create_return_value_constraint((intptr_t)(value))
+#define will_return_by_value(value, size) __CGREEN_NAMESPACE_PREFIX create_return_by_value_constraint((intptr_t)&(value), size)
+#define will_return_double(value) __CGREEN_NAMESPACE_PREFIX create_return_double_value_constraint(value)
+#define will_set_contents_of_parameter(parameter_name, pointer_to_value, size) __CGREEN_NAMESPACE_PREFIX create_set_parameter_value_constraint(#parameter_name, (intptr_t)(pointer_to_value), (size_t)(size))
+#define will_set_contents_of_output_parameter(parameter_name, pointer_to_value, size) __CGREEN_NAMESPACE_PREFIX create_set_parameter_value_constraint(#parameter_name, (intptr_t)(pointer_to_value), (size_t)(size))
+#define will_capture_parameter(parameter_name, local_variable) __CGREEN_NAMESPACE_PREFIX create_capture_parameter_constraint(#parameter_name, &(local_variable), sizeof(local_variable))
 
 #endif
